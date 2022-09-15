a1684e71bef40119ecb8949c84c37d41f98b5264
Nathan Challen <nathan@payninja.co>
2022-09-16 08:22:08 +1000

    Basic timer

--- amaDAO/ama-timer/lib/main.dart
+++ amaDAO/ama-timer/lib/main.dart
@@ -1,27 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:circular_countdown_timer/circular_countdown_timer.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'AMA',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.grey,
       ),
       home: const MyHomePage(title: 'AMA'),
@@ -30,102 +20,176 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({Key? key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  final String? title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final int _duration = 5*60;
+  int _tasks = 1;
 
-  void _incrementCounter() {
+  final CountDownController _controller = CountDownController();
+
+  void _addTask() {
     setState(() {
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
-      _counter++;
+      _tasks++;
     });
   }
 
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(widget.title!),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            Container(
-                margin: EdgeInsets.all(50),
-                child: CircularProgressIndicator(
-                  backgroundColor: Colors.grey,
-                  color: Colors.green,
-                  strokeWidth: 5,
-                  value: _counter / 100.0,
-                ),
-              ),
-            // const Button(
-            //   'reset'
-            // ),
-            // Button (
-            //     style: Theme.of(context),
-            // )
-          ],
+        child: CircularCountDownTimer(
+          // Countdown duration in Seconds.
+          duration: _duration,
 
+          // Countdown initial elapsed Duration in Seconds.
+          initialDuration: 0,
+
+          // Controls (i.e Start, Pause, Resume, Restart) the Countdown Timer.
+          controller: _controller,
+
+          // Width of the Countdown Widget.
+          width: MediaQuery.of(context).size.width / 2,
+
+          // Height of the Countdown Widget.
+          height: MediaQuery.of(context).size.height / 2,
+
+          // Ring Color for Countdown Widget.
+          ringColor: Colors.grey[300]!,
+
+          // Ring Gradient for Countdown Widget.
+          ringGradient: null,
+
+          // Filling Color for Countdown Widget.
+          fillColor: Colors.blueAccent[100]!,
+
+          // Filling Gradient for Countdown Widget.
+          fillGradient: null,
+
+          // Background Color for Countdown Widget.
+          backgroundColor: Colors.blue[500],
+
+          // Background Gradient for Countdown Widget.
+          backgroundGradient: null,
+
+          // Border Thickness of the Countdown Ring.
+          strokeWidth: 20.0,
+
+          // Begin and end contours with a flat edge and no extension.
+          strokeCap: StrokeCap.round,
+
+          // Text Style for Countdown Text.
+          textStyle: const TextStyle(
+            fontSize: 33.0,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+
+          // Format for the Countdown Text.
+          textFormat: CountdownTextFormat.MM_SS,
+
+          // Handles Countdown Timer (true for Reverse Countdown (max to 0), false for Forward Countdown (0 to max)).
+          isReverse: true,
+
+          // Handles Animation Direction (true for Reverse Animation, false for Forward Animation).
+          isReverseAnimation: false,
+
+          // Handles visibility of the Countdown Text.
+          isTimerTextShown: true,
+
+          // Handles the timer start.
+          autoStart: false,
+
+          // This Callback will execute when the Countdown Starts.
+          onStart: () {
+            // Here, do whatever you want
+            debugPrint('Countdown Started');
+          },
+
+          // This Callback will execute when the Countdown Ends.
+          onComplete: () {
+            // Here, do whatever you want
+            debugPrint('Countdown Ended');
+          },
+
+          // This Callback will execute when the Countdown Changes.
+          onChange: (String timeStamp) {
+            // Here, do whatever you want
+            debugPrint('Countdown Changed $timeStamp');
+          },
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: () => _addTask,
+      //   tooltip: 'Add Task',
+      //   child: const Icon(Icons.add),
+      // ), 
+
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+
+
+          const SizedBox(
+            width: 30,
+          ),
+          _button(
+            title: "Start",
+            onPressed: () => _controller.start(),
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+          _button(
+            title: "Pause",
+            onPressed: () => _controller.pause(),
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+          _button(
+            title: "Resume",
+            onPressed: () => _controller.resume(),
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+          _button(
+            title: "Restart",
+            onPressed: () => _controller.restart(duration: _duration),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _button({required String title, VoidCallback? onPressed}) {
+    return Expanded(
+      child: ElevatedButton(
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(Colors.purple),
+        ),
+        onPressed: onPressed,
+        child: Text(
+          title,
+          style: const TextStyle(color: Colors.white),
+        ),
+      ),
     );
   }
 }
