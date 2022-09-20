9a3fc48241ac7862e576574557ab35e6d87a36ff
Tathagat Saha <tathagat.saha@gmail.com>
2022-09-20 20:53:18 +0530

    hardhar config

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -1,6 +1,31 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomiclabs/hardhat-waffle')
+require('@nomiclabs/hardhat-etherscan')
+require('@openzeppelin/hardhat-upgrades')
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const infura_projectId = '403f2033226a44788c2638cc1c29d438'
+const fs = require('fs')
+const privateKey = fs.readFileSync('.secret').toString()
+
 module.exports = {
-  solidity: "0.8.9",
-};
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+
+    celo_testnet: {
+      url: `https://alfajores-forno.celo-testnet.org`,
+      accounts: [privateKey],
+    },
+    celo_mainnet: {
+      url: `https://forno.celo.org`,
+      accounts: [privateKey],
+    }
+  },
+  etherscan: {
+    apiKey: '5PB2QWEDWRA9JBUWGBDHHZFM2X5YECC5Q2',
+  },
+  solidity: '0.8.2',
+  paths: {
+    artifacts: '../web/artifacts',
+  },
+}
