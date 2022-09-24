95b413bb15dff719183f53654e272bfe6d273d6a
Kachi <kachdekan@gmail.com>
2022-09-24 16:13:52 +0300

    initail expo commint

---
+++ CLIXPESA/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Keep Calm!</Text>
+      <Text>We building something beautiful!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
