7f6003daafefa980cf8ce8333dd646bdc6d8f716
Chebotov Nikolay <spiritkola@hotmail.com>
2022-09-27 14:33:33 +0300

    update: Add "Nomis.Blockchain.Abstractions" and "Nomis.Celoscan.Interfaces"

---
+++ Nomis/src/Nomis.Blockchain.Abstractions/IWalletStats.cs
@@ -0,0 +1,78 @@
+﻿namespace Nomis.Blockchain.Abstractions
+{
+    /// <summary>
+    /// Wallet stats.
+    /// </summary>
+    public interface IWalletStats
+    {
+        /// <summary>
+        /// Wallet balance (Native token).
+        /// </summary>
+        public decimal Balance { get; set; }
+
+        /// <summary>
+        /// Wallet age (months).
+        /// </summary>
+        public int WalletAge { get; set; }
+
+        /// <summary>
+        /// Total transactions on wallet (number).
+        /// </summary>
+        public int TotalTransactions { get; set; }
+
+        /// <summary>
+        /// Average time interval between transactions (hours).
+        /// </summary>
+        public double AverageTransactionTime { get; set; }
+
+        /// <summary>
+        /// Maximum time interval between transactions (hours).
+        /// </summary>
+        public double MaxTransactionTime { get; set; }
+
+        /// <summary>
+        /// Minimal time interval between transactions (hours).
+        /// </summary>
+        public double MinTransactionTime { get; set; }
+
+        /// <summary>
+        /// The movement of funds on the wallet (Native token).
+        /// </summary>
+        public decimal WalletTurnover { get; set; }
+
+        /// <summary>
+        /// Total NFTs on wallet (number).
+        /// </summary>
+        public int NftHolding { get; set; }
+
+        /// <summary>
+        /// Time since last transaction (months).
+        /// </summary>
+        public int TimeFromLastTransaction { get; set; }
+
+        /// <summary>
+        /// NFT trading activity (Native token).
+        /// </summary>
+        public decimal NftTrading { get; set; }
+
+        /// <summary>
+        /// NFT worth on wallet (Native token).
+        /// </summary>
+        public decimal NftWorth { get; set; }
+
+        /// <summary>
+        /// Average transaction per months (number).
+        /// </summary>
+        public double TransactionsPerMonth { get; }
+
+        /// <summary>
+        /// Last month transactions (number).
+        /// </summary>
+        public int LastMonthTransactions { get; set; }
+
+        /// <summary>
+        /// Value of all holding tokens (number).
+        /// </summary>
+        public int TokensHolding { get; set; }
+    }
+}
---
+++ Nomis/src/Nomis.Blockchain.Abstractions/Settings/ApiVisibilitySettings.cs
@@ -0,0 +1,16 @@
+﻿using Nomis.Utils.Contracts.Common;
+
+namespace Nomis.Blockchain.Abstractions.Settings
+{
+    /// <summary>
+    /// API visibility settings.
+    /// </summary>
+    public class ApiVisibilitySettings
+        : ISettings
+    {
+        /// <summary>
+        /// Celo API is enabled.
+        /// </summary>
+        public bool CeloAPIEnabled { get; set; }
+    }
+}
