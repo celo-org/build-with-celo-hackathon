7f60e572e878a3eac518316d20d63e87e2362c1a
Chebotov Nikolay <spiritkola@hotmail.com>
2022-09-28 12:20:47 +0300

    update: Add CeloWalletScore and CeloWalletStats

---
+++ Nomis/src/Nomis.Celoscan.Interfaces/Models/CeloWalletScore.cs
@@ -0,0 +1,18 @@
+﻿namespace Nomis.Celoscan.Interfaces.Models
+{
+    /// <summary>
+    /// Celo wallet score.
+    /// </summary>
+    public class CeloWalletScore
+    {
+        /// <summary>
+        /// Nomis Score in range of [0; 1].
+        /// </summary>
+        public double Score { get; set; }
+
+        /// <summary>
+        /// Additional stat data used in score calculations.
+        /// </summary>
+        public CeloWalletStats? Stats { get; set; }
+    }
+}
---
+++ Nomis/src/Nomis.Celoscan.Interfaces/Models/CeloWalletStats.cs
@@ -0,0 +1,80 @@
+﻿using System.ComponentModel;
+
+using Nomis.Blockchain.Abstractions;
+
+namespace Nomis.Celoscan.Interfaces.Models
+{
+    /// <summary>
+    /// Celo wallet stats.
+    /// </summary>
+    public class CeloWalletStats
+        : IWalletStats
+    {
+        /// <summary>
+        /// No data.
+        /// </summary>
+        public bool NoData { get; init; }
+
+        /// <summary>
+        /// Amount of deployed smart-contracts (number).
+        /// </summary>
+        [Description("Amount of deployed smart-contracts (number)")]
+        public int DeployedContracts { get; init; }
+
+        /// <inheritdoc/>
+        [Description("Wallet balance (CELO)")]
+        public decimal Balance { get; set; }
+
+        /// <inheritdoc/>
+        [Description("Wallet age (months)")]
+        public int WalletAge { get; set; }
+
+        /// <inheritdoc/>
+        [Description("Total transactions on wallet (number)")]
+        public int TotalTransactions { get; set; }
+
+        /// <inheritdoc/>
+        [Description("Average time interval between transactions (hours)")]
+        public double AverageTransactionTime { get; set; }
+
+        /// <inheritdoc/>
+        [Description("Maximum time interval between transactions (hours)")]
+        public double MaxTransactionTime { get; set; }
+
+        /// <inheritdoc/>
+        [Description("Minimal time interval between transactions (hours)")]
+        public double MinTransactionTime { get; set; }
+
+        /// <inheritdoc/>
+        [Description("The movement of funds on the wallet (CELO)")]
+        public decimal WalletTurnover { get; set; }
+
+        /// <inheritdoc/>
+        [Description("Total NFTs on wallet (number)")]
+        public int NftHolding { get; set; }
+
+        /// <inheritdoc/>
+        [Description("Time since last transaction (months)")]
+        public int TimeFromLastTransaction { get; set; }
+
+        /// <inheritdoc/>
+        [Description("NFT trading activity (CELO)")]
+        public decimal NftTrading { get; set; }
+
+        /// <inheritdoc/>
+        [Description("NFT worth on wallet (CELO)")]
+        public decimal NftWorth { get; set; }
+
+        /// <inheritdoc/>
+        [Description("Last month transactions (number)")]
+        public int LastMonthTransactions { get; set; }
+
+        /// <inheritdoc/>
+        [Description("Average transaction per months (number)")]
+        public double TransactionsPerMonth => WalletAge != 0 ? (double)TotalTransactions / WalletAge : 0;
+
+        /// <inheritdoc/>
+        [Description("Value of all holding tokens (number)")]
+        public int TokensHolding { get; set; }
+    }
+}
