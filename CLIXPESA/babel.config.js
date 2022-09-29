1013a2a4a4210b5bffe7da373dea14ba0005452c
Kachi <kachdekan@gmail.com>
2022-09-29 17:54:22 +0300

    some updates

--- CLIXPESA/babel.config.js
+++ CLIXPESA/babel.config.js
@@ -1,6 +1,21 @@
-module.exports = function(api) {
-  api.cache(true);
+module.exports = function (api) {
+  api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-  };
-};
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            '@native-base/icons': '@native-base/icons/lib',
+            'clixpesa/app': './app',
+            'clixpesa/blockchain': './blockchain',
+            'clixpesa/components': './components',
+            'clixpesa/features': './features',
+            'clixpesa/utils': './utils',
+          },
+        },
+      ],
+    ],
+  }
+}
