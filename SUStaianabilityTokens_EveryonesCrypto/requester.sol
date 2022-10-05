6c19b53fae5669b2c8179e571c48e2cbed6767bb
munivenkateswarluk <mkumaramangalam@catalystreaction.com>
2022-10-05 12:55:38 -0600

    Oracle consumption requester

---
+++ SUStaianabilityTokens_EveryonesCrypto/requester.sol
@@ -0,0 +1,45 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.9;
+
+import "@api3/airnode-protocol/contracts/rrp/requesters/RrpRequesterV0.sol";
+
+// A requester that will return the Tesla Stock Price by calling the dxFeed airnode.
+
+contract Requester is RrpRequesterV0 {
+    mapping(bytes32 => bool) public incomingFulfillments;
+    mapping(bytes32 => int256) public fulfilledData;
+
+    constructor(address _rrpAddress) RrpRequesterV0(_rrpAddress) {}
+
+// The main makeRequest function that will trigger the Airnode request.
+
+    function makeRequest(
+        address airnode,
+        bytes32 endpointId,
+        address sponsor,
+        address sponsorWallet,
+        bytes calldata parameters
+        
+    ) external {
+        bytes32 requestId = airnodeRrp.makeFullRequest(
+            airnode,
+            endpointId,
+            sponsor,
+            sponsorWallet,
+            address(this),
+            this.fulfill.selector,
+            parameters
+        );
+        incomingFulfillments[requestId] = true;
+    }
+
+    function fulfill(bytes32 requestId, bytes calldata data)
+        external
+        onlyAirnodeRrp
+    {
+        require(incomingFulfillments[requestId], "No such request made");
+        delete incomingFulfillments[requestId];
+        int256 decodedData = abi.decode(data, (int256));
+        fulfilledData[requestId] = decodedData;
+    }
+}
