f5347e10d26a339cad8a31210cf81f4b21b2d3f3
SANKARAMOORTHY-DEVELOPER <78921146+SANKARAMOORTHY-DEVELOPER@users.noreply.github.com>
2022-10-06 03:04:15 +0530

    nft minting algorithm

---
+++ CNFT/contracts/algorithm/cnft.sol
@@ -0,0 +1,7 @@
+//SPDX-License-Identifier: GPL-3.0
+
+pragma solidity^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+//simple nft minting cnft phase 1.0.3 
