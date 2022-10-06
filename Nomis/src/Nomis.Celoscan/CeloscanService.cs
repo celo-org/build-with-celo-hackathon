d771c89efd3ebc932ef1d9c70766b869704e5f71
Chebotov Nikolay <spiritkola@hotmail.com>
2022-10-06 13:38:04 +0300

    update(Celoscan): Add CeloscanService (WIP)

---
+++ Nomis/src/Nomis.Celoscan/CeloscanService.cs
@@ -0,0 +1,34 @@
+﻿using Nomis.Celoscan.Interfaces;
+using Nomis.Celoscan.Interfaces.Models;
+using Nomis.Utils.Contracts.Services;
+using Nomis.Utils.Wrapper;
+
+namespace Nomis.Celoscan
+{
+    /// <inheritdoc cref="ICeloscanService"/>
+    internal sealed class CeloscanService :
+        ICeloscanService,
+        ITransientService
+    {
+        /// <summary>
+        /// Initialize <see cref="CeloscanService"/>.
+        /// </summary>
+        /// <param name="client"><see cref="ICeloscanClient"/>.</param>
+        public CeloscanService(
+            ICeloscanClient client)
+        {
+            Client = client;
+        }
+
+        /// <inheritdoc/>
+        public ICeloscanClient Client { get; }
+
+        /// <inheritdoc/>
+        public async Task<Result<CeloWalletScore>> GetWalletStatsAsync(string address)
+        {
+            // TODO - implement
+
+            return new();
+        }
+    }
+}
