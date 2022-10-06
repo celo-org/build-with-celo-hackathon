5488813b83d7653f86be12a3bd8cdc419d674379
= <=>
2022-10-06 21:30:23 +0100

    added a section

--- packages/react-app/pages/index.js
+++ packages/react-app/pages/index.js
@@ -38,9 +38,107 @@ const Home = () => {
                
             </section>  
 
-            <section>
-                <div className=''>
+            <section className='py-4 bg-[#FFFBF9]'>
+                <div className='container mx-auto'>
+                    <div className='py-14'>
+                        <div className='mb-9'>
+                            <h3 className='text-center text-xl'>How it Works</h3>
+                        </div>
+                        
+                        <div>
+                            <div className='flex items-center justify-center gap-10'>
+                                <button className="">Company</button>
+                                <button className="">Individual</button>
+                            </div>
 
+                            <div className='tab-content py-10'>
+                                <div className='grid grid-cols-4'>
+                                    <div className='px-5'>
+                                        <div>
+                                            <div className='circle'></div>
+                                        </div>
+                                        <div className='text-center'>
+                                            <h4>Create Account</h4>
+                                            <p className='text-sm'>Company Staff creates an account on GidiScrap with MetaMask (or Valora)</p>
+                                        </div>
+                                    </div>
+                                    <div className='px-5'>
+                                        <div>
+                                            <div className='circle'></div>
+                                        </div>
+                                        <div className='text-center'>
+                                            <h4>Create Account</h4>
+                                            <p className='text-sm'>Company Staff creates an account on GidiScrap with MetaMask (or Valora)</p>
+                                        </div>
+                                    </div>
+                                    <div className='px-5'>
+                                        <div>
+                                            <div className='circle'></div>
+                                        </div>
+                                        <div className='text-center'>
+                                            <h4>Create Account</h4>
+                                            <p className='text-sm'>Company Staff creates an account on GidiScrap with MetaMask (or Valora)</p>
+                                        </div>
+                                    </div>
+                                    <div className='px-5'>
+                                        <div>
+                                            <div className='circle'></div>
+                                        </div>
+                                        <div className='text-center'>
+                                            <h4>Create Account</h4>
+                                            <p className='text-sm'>Company Staff creates an account on GidiScrap with MetaMask (or Valora)</p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+            </section>
+
+
+            <section className='py-4 bg-[#FFFBF9]'>
+                <div className='container mx-auto'>
+                    <div className='py-14'>
+                        <div className='mb-9'>
+                            <h3 className='text-center text-xl'>Recent Requests</h3>
+                        </div>
+                        
+                        <div>
+
+                            <div className='grid grid-cols-4'>
+                                <div className='px-5'>
+                                    <div>
+                                        <div className='w-full h-56'>
+                                            <img src='/images/water_bottle.png' className='w-full h-full'/>
+                                        </div>
+                                    </div>
+                                    <div className=' flex items-start justify-between'>
+                                       <div>
+                                            <h4>PET Bottles</h4>
+                                            <div>
+                                                <img src=''/>
+                                                <div>
+                                                    <p className=''>Ikeja, Lagos</p>
+                                                    <p>20 mins away from you</p>
+                                                </div>
+                                            
+                                            </div> 
+                                            
+                                       </div>
+                                        <div>
+                                            circle
+                                        </div>
+                                        
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                    
                 </div>
             </section>
         
