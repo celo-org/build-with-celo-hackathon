21921ae1b081da74541481c0368244b82b3e7c35
oryankibandi <shreckoryan@gmail.com>
2022-10-09 02:28:55 +0300

    Added functionality of the smart contract

--- Crypto_Ventures/Backend/truffle-config.js
+++ Crypto_Ventures/Backend/truffle-config.js
@@ -7,7 +7,7 @@
  * More information about configuration can be found at:
  *
  * https://trufflesuite.com/docs/truffle/reference/configuration
- * 
+ *
  * Hands-off deployment with Infura
  * --------------------------------
  *
@@ -15,29 +15,29 @@
  * Use this approach to make deployment a breeze 🏖️:
  *
  * Infura deployment needs a wallet provider (like @truffle/hdwallet-provider)
- * to sign transactions before they're sent to a remote public node. 
+ * to sign transactions before they're sent to a remote public node.
  * Infura accounts are available for free at 🔍: https://infura.io/register
  *
  * You'll need a mnemonic - the twelve word phrase the wallet uses to generate
- * public/private key pairs. You can store your secrets 🤐 in a .env file. 
- * In your project root, run `$ npm install dotenv`. 
- * Create .env (which should be .gitignored) and declare your MNEMONIC 
+ * public/private key pairs. You can store your secrets 🤐 in a .env file.
+ * In your project root, run `$ npm install dotenv`.
+ * Create .env (which should be .gitignored) and declare your MNEMONIC
  * and Infura PROJECT_ID variables inside.
  * For example, your .env file will have the following structure:
- * 
+ *
  * MNEMONIC = <Your 12 phrase mnemonic>
  * PROJECT_ID = <Your Infura project id>
- * 
+ *
  * Deployment with Truffle Dashboard (Recommended for best security practice)
  * --------------------------------------------------------------------------
- * 
+ *
  * Are you concerned about security and minimizing rekt status 🤔?
  * Use this method for best security:
- * 
- * Truffle Dashboard lets you review transactions in detail, and leverages 
- * MetaMask for signing, so there's no need to copy-paste your mnemonic. 
- * More details can be found at 🔎: 
- * 
+ *
+ * Truffle Dashboard lets you review transactions in detail, and leverages
+ * MetaMask for signing, so there's no need to copy-paste your mnemonic.
+ * More details can be found at 🔎:
+ *
  * https://trufflesuite.com/docs/truffle/getting-started/using-the-truffle-dashboard/
  */
 
@@ -64,11 +64,11 @@ module.exports = {
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
     //
-    // development: {
-    //  host: "127.0.0.1",     // Localhost (default: none)
-    //  port: 8545,            // Standard Ethereum port (default: none)
-    //  network_id: "*",       // Any network (default: none)
-    // },
+    development: {
+      host: "127.0.0.1", // Localhost (default: none)
+      port: 7545, // Standard Ethereum port (default: none)
+      network_id: "*", // Any network (default: none)
+    },
     //
     // An additional network, but with some advanced options…
     // advanced: {
@@ -103,19 +103,14 @@ module.exports = {
     // timeout: 100000
   },
 
+  contracts_directory: "./contracts",
+  contracts_build_directory: "./abis",
+
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.8.17",      // Fetch exact version from solc-bin (default: truffle's version)
-      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
-      //  evmVersion: "byzantium"
-      // }
-    }
+      version: "0.8.17",
+    },
   },
 
   // Truffle DB is currently disabled by default; to enable it, change enabled:
