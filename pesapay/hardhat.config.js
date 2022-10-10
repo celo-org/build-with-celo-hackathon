8fb95242e2bcc64f9143c688ef9470667e8cc6b1
eliashezron <eliashezron23@gmail.com>
2022-10-10 14:26:12 +0000

    added custom celo and alfajores chains

--- pesapay/hardhat.config.js
+++ pesapay/hardhat.config.js
@@ -26,6 +26,24 @@ module.exports = {
     apiKey: {
       alfajores: process.env.CELOSCAN_API_KEY,
     },
+    customChains: [
+      {
+        network: "alfajores",
+        chainId: 44787,
+        urls: {
+          apiURL: "https://api-alfajores.celoscan.io/api",
+          browserURL: "https://alfajores.celoscan.io",
+        },
+      },
+      {
+        network: "celo",
+        chainId: 42220,
+        urls: {
+          apiURL: "https://api.celoscan.io/api",
+          browserURL: "https://celoscan.io/",
+        },
+      },
+    ],
   },
   networks: {
     alfajores: {
