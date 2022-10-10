120d81da8c5a11d8c2340b32bdd53d8719991dbd
eliashezron <eliashezron23@gmail.com>
2022-10-10 19:03:33 +0000

    finally verified the contract

--- pesapay/hardhat.config.js
+++ pesapay/hardhat.config.js
@@ -1,7 +1,8 @@
-require("@nomiclabs/hardhat-waffle")
+require("@nomicfoundation/hardhat-chai-matchers")
 require("@openzeppelin/hardhat-upgrades")
 require("@nomiclabs/hardhat-ethers")
 require("@nomiclabs/hardhat-etherscan")
+require("hardhat-celo")
 require("dotenv").config()
 
 // This is a sample Hardhat task. To learn how to create your own go to
@@ -25,31 +26,37 @@ module.exports = {
   etherscan: {
     apiKey: {
       alfajores: process.env.CELOSCAN_API_KEY,
+      celo: process.env.CELOSCAN_API_KEY,
     },
-    customChains: [
-      {
-        network: "alfajores",
-        chainId: 44787,
-        urls: {
-          apiURL: "https://api-alfajores.celoscan.io/api",
-          browserURL: "https://alfajores.celoscan.io",
-        },
-      },
-      {
-        network: "celo",
-        chainId: 42220,
-        urls: {
-          apiURL: "https://api.celoscan.io/api",
-          browserURL: "https://celoscan.io/",
-        },
-      },
-    ],
+    // customChains: [
+    //   {
+    //     network: "alfajores",
+    //     chainId: 44787,
+    //     urls: {
+    //       apiURL: "https://api-alfajores.celoscan.io/api",
+    //       browserURL: "https://alfajores.celoscan.io",
+    //     },
+    //   },
+    //   {
+    //     network: "celo",
+    //     chainId: 42220,
+    //     urls: {
+    //       apiURL: "https://api.celoscan.io/api",
+    //       browserURL: "https://celoscan.io/",
+    //     },
+    //   },
+    // ],
   },
   networks: {
     alfajores: {
       url: "https://alfajores-forno.celo-testnet.org",
       accounts: [process.env.PRIVATE_KEY],
       chainId: 44787,
+    },
+    celo: {
+      url: "https://forno.celo.org",
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 42220,
     },
   },
 }
