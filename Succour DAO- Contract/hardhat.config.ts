e874424416067aeac571bdda6b4d9e3031bc6f0a
Adebara Ayomide <adebaraayomide.com>
2022-10-14 15:14:06 +0100

    Update on smart contract

--- Succour DAO- Contract/hardhat.config.ts
+++ Succour DAO- Contract/hardhat.config.ts
@@ -1,8 +1,26 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import { any } from "hardhat/internal/core/params/argumentTypes";
+require("dotenv").config();
 
+type HttpNetworkAccountsUserConfig = any;
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: "0.8.9",
+  networks: {
+    rinkeby: {
+      url: process.env.RINKEBY_URL,
+      accounts: [process.env.PRIVATE_KEY] as HttpNetworkAccountsUserConfig | undefined,
+    },
+    goerli: {
+      url: process.env.GOERLI_URL,
+      accounts: [process.env.PRIVATE_KEY] as HttpNetworkAccountsUserConfig | undefined,
+    },
+    ropsten: {
+      url: process.env.ROPSTEN_URL,
+      accounts: [process.env.PRIVATE_KEY] as HttpNetworkAccountsUserConfig | undefined,
+    }
+  }
 };
 
 export default config;
+
