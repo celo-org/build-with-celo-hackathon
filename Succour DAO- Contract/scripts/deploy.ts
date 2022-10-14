498ac8ef84a28d5963cd9bd55d52cada388d0781
Adebara Ayomide <adebaraayomide.com>
2022-10-14 17:28:55 +0100

    [CHORE]- Created the deploy script

--- Succour DAO- Contract/scripts/deploy.ts
+++ Succour DAO- Contract/scripts/deploy.ts
@@ -1,18 +1,15 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  
 
-  const lockedAmount = ethers.utils.parseEther("1");
+  console.log("Deploying Succour contract")
+  const Succour = await ethers.getContractFactory("Succour");
+  const succour = await Succour.deploy();
+  await succour.deployed();
+  console.log(`Succour contract deployed to ${succour.address}`);
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+  
 }
 
 // We recommend this pattern to be able to use async/await everywhere
