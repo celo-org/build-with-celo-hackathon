a6c30023b5f0e86bccc459ac94d2a90302310e7c
auma2022 <dianaauma2@gmail.com>
2022-10-14 14:40:02 +0300

    add rails

--- Crypto_Ventures/frontend/components/Navbar.jsx
+++ Crypto_Ventures/frontend/components/Navbar.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
 import Link from 'next/link';
+import React, { useState, useEffect } from 'react';
 import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
 
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const [color, setColor] = useState('transparent');
+  const [textColor, setTextColor] = useState('white');
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
+  useEffect(() => {
+    const changeColor = () => {
+      if (window.scrollY >= 90) {
+        setColor('#ffffff');
+        setTextColor('#000000');
+      } else {
+        setColor('transparent');
+        setTextColor('#ffffff');
+      }
+    };
+    window.addEventListener('scroll', changeColor);
+  }, []);
 
 function Navbar() {
   return (
@@ -23,19 +44,26 @@ function Navbar() {
         </ul>
 
         
-        <div className='block sm:hidden z-10'>
+        {/* <div className='block sm:hidden z-10'>
           <AiOutlineMenu size={20} />
-          </div>
+          </div> */}
+          <div onClick={handleNav} className='block sm:hidden z-10'>
+          {nav ? (
+            <AiOutlineClose size={20} style={{ color: `${textColor}` }} />
+          ) : (
+            <AiOutlineMenu size={20} style={{ color: `${textColor}` }} />
+          )}
+        </div>
         <div className='
               sm:hidden absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300'>
            <ul>
-          <li>
+          <li className='p-4 text-4xl hover:text-grey-500'>
             <Link href='/'>Home</Link>
           </li>
-          <li>
+          <li className='p-4 text-4xl hover:text-grey-500'> 
             <Link href='/startups'>Startups</Link>
           </li>
-          <li>
+          <li className='p-4 text-4xl hover:text-grey-500'>
             <Link href='/funding'>Funding</Link>
           </li>
         </ul>
@@ -44,6 +72,7 @@ function Navbar() {
       </div>
     </div>
   )
+}
 }
 
 export default Navbar
