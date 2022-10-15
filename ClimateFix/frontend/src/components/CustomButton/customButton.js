94ad26084e9a96c7889b1281d8cb9041bfb4a109
Temitope3665 <aroyewontope@gmail.com>
2022-10-15 22:26:07 +0100

    added how it works section on the landing page

--- ClimateFix/frontend/src/components/CustomButton/customButton.js
+++ ClimateFix/frontend/src/components/CustomButton/customButton.js
@@ -1,8 +1,8 @@
 import { Button } from "@chakra-ui/react";
 
-const CustomButton = ({ bg, color, children, border, hoverBg, hoverColor, m }) => {
+const CustomButton = ({ bg, color, children, border, hoverBg, hoverColor, m, p }) => {
   return (
-    <Button background={bg} color={color} borderRadius="4rem" p="8px 24px" _hover={{ bg: hoverBg, color: hoverColor}} border={border} style={{ transition: "all 0.8s ease" }} fontSize="14px" margin={m}>
+    <Button background={bg} color={color} borderRadius="4rem" p="8px 24px" _hover={{ bg: hoverBg, color: hoverColor}} border={border} style={{ transition: "all 0.8s ease" }} fontSize="14px" margin={m} padding={p}>
       {children}
     </Button>
   );
--- ClimateFix/frontend/src/pages/LandingPage.js
+++ ClimateFix/frontend/src/pages/LandingPage.js
@@ -1,8 +1,10 @@
-import { Box, Flex, Image, Text } from "@chakra-ui/react";
+import { Box, Flex, Image, SimpleGrid, Text } from "@chakra-ui/react";
 import NavBar from "../components/Navbar/navbar";
-import background from "..//assets/images/background-img.png";
 import { coinIcon } from "../assets/svgs/svg";
 import CustomButton from "../components/CustomButton/customButton";
+import PlantTrees from "../assets/icons/plant-trees.svg";
+import ExpertConnects from "../assets/icons/connect-with-experts.svg";
+import EarnTokens from "../assets/icons/earn-some.svg";
 
 const LandingPage = () => {
     return (
@@ -25,7 +27,7 @@ const LandingPage = () => {
                             <CustomButton border="1px solid #FAF9F7" bg="none" color="white" hoverBg="brand.secondary">Learn more</CustomButton>
                             <CustomButton bg="brand.orange" color="white" hoverBg="brand.secondary" m="0 20px">Sign up</CustomButton>
                         </Flex>
-                    <Flex fontSize="12px" justifyContent="space-around" w="100%" textAlign="center" bottom="0" pos="absolute" p="20px 0">
+                    <Flex fontSize="12px" justifyContent="space-around" w="100%" textAlign="center" bottom="0" pos="absolute" p="40px 0">
                         <Box>
                             <Text color="brand.orange">10,000</Text>
                             <Text color="brand.white">Agricultural Experts</Text>
@@ -45,6 +47,49 @@ const LandingPage = () => {
                     </Flex>
                 </Box>
             </main>
+
+            <section>
+                <Box bg="brand.lightGrey">
+                    <Text p="40px 80px" fontSize="50px" fontWeight="extrabold" color="brand.dark" textAlign="center">How it Works</Text>
+                    <SimpleGrid columns={3} spacing="40" textAlign="center" p="40px 80px">
+                        <Box margin="0 auto" w="100%">
+                            <Image src={PlantTrees} alt="plant-trees" m="0 auto" />
+                            <Box mt="20px">
+                                <Text fontWeight="900">Plant Trees</Text>
+                                <Text fontSize="14px" mt="10px">
+                                    Find a suitable location
+                                    to grow trees and improve the
+                                    ecosystem
+                                </Text>
+                            </Box>
+                        </Box>
+                        <Box margin="0 auto" w="100%">
+                            <Image src={ExpertConnects} alt="connect-with-experts" m="0 auto" />
+                            <Box mt="10px">
+                                <Text fontWeight="900">Connect With an Experts</Text>
+                                <Text fontSize="14px" mt="10px">
+                                Get connected with an experts to help you improve the lives of the trees you planted
+                                </Text>
+                            </Box>
+                        </Box>
+                        <Box margin="0 auto" w="100%">
+                            <Image src={EarnTokens} alt="connect-with-experts" m="0 auto" />
+                            <Box mt="20px">
+                                <Text fontWeight="900">Earn Some Tokens</Text>
+                                <Text fontSize="14px" mt="10px">
+                                Earn some dollars when you
+                                plant trees at a suitable environment
+                                where it can grow.
+                                </Text>
+                            </Box>
+                        </Box>
+                    </SimpleGrid>
+                    <Flex w="100%">
+                        <CustomButton m="30px auto" border="1px solid #212529" bg="none" color="brand.dark" hoverBg="brand.secondary">View more!</CustomButton>
+                    </Flex>
+                </Box>
+
+            </section>
         </Box>
     );
 };
