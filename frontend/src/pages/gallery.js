03d4aee63af98d4b698ad23e64c5caad32bbf978
sandeep bhowmik <bhowmik.was.sandeep8@gmail.com>
2022-10-16 09:45:56 +0100

    Gallary

--- frontend/src/pages/gallery.js
+++ frontend/src/pages/gallery.js
@@ -1,132 +1,75 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import BreadCrumb from '../components/breadcrumb';
 import Fancybox from '../components/Fancybox';
+import { client, urlFor } from "../lib/sanityClient"; 
+import { Dna } from 'react-loader-spinner'
 
-export default class Gallery extends Component {
-    render() {
-        return (
-            <div>
-                <BreadCrumb imageURL="/asssets/images/bg_2.jpg" pagename="Galleries" pageURL="GALLERY" />
-                <section class="ftco-section ftco-gallery">
-                    <div class="container">
-                        <div class="d-md-flex">
-                            <Fancybox options={{ infinite: false }}>
-                                <button class="gallery image-popup d-flex justify-content-center align-items-center img" data-fancybox="gallery"
-                                    style={{ backgroundImage: `url(${process.env.PUBLIC_URL + '/asssets/images/cause-2.jpg'})` }}
-                                    data-src={process.env.PUBLIC_URL + '/asssets/images/cause-2.jpg'}>
-                                    <div class="icon d-flex justify-content-center align-items-center">
-                                        <span class="icon-search"></span>
-                                    </div>
-                                </button>
-                            </Fancybox>
-                            <Fancybox options={{ infinite: false }}>
-                                <button class="gallery image-popup d-flex justify-content-center align-items-center img" data-fancybox="gallery"
-                                    style={{ backgroundImage: `url(${process.env.PUBLIC_URL + '/asssets/images/cause-3.jpg'})` }}
-                                    data-src={process.env.PUBLIC_URL + '/asssets/images/cause-3.jpg'}>
-                                    <div class="icon d-flex justify-content-center align-items-center">
-                                        <span class="icon-search"></span>
-                                    </div>
-                                </button>
-                            </Fancybox>
-                            <Fancybox options={{ infinite: false }}>
-                                <button class="gallery image-popup d-flex justify-content-center align-items-center img" data-fancybox="gallery"
-                                    style={{ backgroundImage: `url(${process.env.PUBLIC_URL + '/asssets/images/cause-4.jpg'})` }}
-                                    data-src={process.env.PUBLIC_URL + '/asssets/images/cause-4.jpg'}>
-                                    <div class="icon d-flex justify-content-center align-items-center">
-                                        <span class="icon-search"></span>
-                                    </div>
-                                </button>
-                            </Fancybox>
-                            <Fancybox options={{ infinite: false }}>
-                                <button class="gallery image-popup d-flex justify-content-center align-items-center img" data-fancybox="gallery"
-                                    style={{ backgroundImage: `url(${process.env.PUBLIC_URL + '/asssets/images/cause-5.jpg'})` }}
-                                    data-src={process.env.PUBLIC_URL + '/asssets/images/cause-5.jpg'}>
-                                    <div class="icon d-flex justify-content-center align-items-center">
-                                        <span class="icon-search"></span>
-                                    </div>
-                                </button>
-                            </Fancybox>
-                        </div>
-                        <div class="d-md-flex">
-                            <Fancybox options={{ infinite: false }}>
-                                <button class="gallery image-popup d-flex justify-content-center align-items-center img" data-fancybox="gallery"
-                                    style={{ backgroundImage: `url(${process.env.PUBLIC_URL + '/asssets/images/event-1.jpg'})` }}
-                                    data-src={process.env.PUBLIC_URL + '/asssets/images/event-1.jpg'}>
-                                    <div class="icon d-flex justify-content-center align-items-center">
-                                        <span class="icon-search"></span>
-                                    </div>
-                                </button>
-                            </Fancybox>
-                            <Fancybox options={{ infinite: false }}>
-                                <button class="gallery image-popup d-flex justify-content-center align-items-center img" data-fancybox="gallery"
-                                    style={{ backgroundImage: `url(${process.env.PUBLIC_URL + '/asssets/images/event-2.jpg'})` }}
-                                    data-src={process.env.PUBLIC_URL + '/asssets/images/event-2.jpg'}>
-                                    <div class="icon d-flex justify-content-center align-items-center">
-                                        <span class="icon-search"></span>
-                                    </div>
-                                </button>
-                            </Fancybox>
-                            <Fancybox options={{ infinite: false }}>
-                                <button class="gallery image-popup d-flex justify-content-center align-items-center img" data-fancybox="gallery"
-                                    style={{ backgroundImage: `url(${process.env.PUBLIC_URL + '/asssets/images/event-3.jpg'})` }}
-                                    data-src={process.env.PUBLIC_URL + '/asssets/images/event-2.jpg'}>
-                                    <div class="icon d-flex justify-content-center align-items-center">
-                                        <span class="icon-search"></span>
-                                    </div>
-                                </button>
-                            </Fancybox>
-                            <Fancybox options={{ infinite: false }}>
-                                <button class="gallery image-popup d-flex justify-content-center align-items-center img" data-fancybox="gallery"
-                                    style={{ backgroundImage: `url(${process.env.PUBLIC_URL + '/asssets/images/event-4.jpg'})` }}
-                                    data-src={process.env.PUBLIC_URL + '/asssets/images/event-4.jpg'}>
-                                    <div class="icon d-flex justify-content-center align-items-center">
-                                        <span class="icon-search"></span>
-                                    </div>
-                                </button>
-                            </Fancybox>
-                        </div>
-                        <div class="d-md-flex">
-                            <Fancybox options={{ infinite: false }}>
-                                <button class="gallery image-popup d-flex justify-content-center align-items-center img" data-fancybox="gallery"
-                                    style={{ backgroundImage: `url(${process.env.PUBLIC_URL + '/asssets/images/event-5.jpg'})` }}
-                                    data-src={process.env.PUBLIC_URL + '/asssets/images/event-5.jpg'}>
-                                    <div class="icon d-flex justify-content-center align-items-center">
-                                        <span class="icon-search"></span>
-                                    </div>
-                                </button>
-                            </Fancybox>
-                            <Fancybox options={{ infinite: false }}>
-                                <button class="gallery image-popup d-flex justify-content-center align-items-center img" data-fancybox="gallery"
-                                    style={{ backgroundImage: `url(${process.env.PUBLIC_URL + '/asssets/images/event-6.jpg'})` }}
-                                    data-src={process.env.PUBLIC_URL + '/asssets/images/event-6.jpg'}>
-                                    <div class="icon d-flex justify-content-center align-items-center">
-                                        <span class="icon-search"></span>
-                                    </div>
-                                </button>
-                            </Fancybox>
-                            <Fancybox options={{ infinite: false }}>
-                                <button class="gallery image-popup d-flex justify-content-center align-items-center img" data-fancybox="gallery"
-                                    style={{ backgroundImage: `url(${process.env.PUBLIC_URL + '/asssets/images/image_2.jpg'})` }}
-                                    data-src={process.env.PUBLIC_URL + '/asssets/images/image_2.jpg'}>
-                                    <div class="icon d-flex justify-content-center align-items-center">
-                                        <span class="icon-search"></span>
-                                    </div>
-                                </button>
-                            </Fancybox>
-                            <Fancybox options={{ infinite: false }}>
-                                <button class="gallery image-popup d-flex justify-content-center align-items-center img" data-fancybox="gallery"
-                                    style={{ backgroundImage: `url(${process.env.PUBLIC_URL + '/asssets/images/image_3.jpg'})` }}
-                                    data-src={process.env.PUBLIC_URL + '/asssets/images/image_3.jpg'}>
-                                    <div class="icon d-flex justify-content-center align-items-center">
-                                        <span class="icon-search"></span>
-                                    </div>
-                                </button>
-                            </Fancybox>
-                        </div>
+const Gallery = () => {
+    const [photos, setPhotos] = useState([])
+    const [contentLoaded, setcontentLoaded] = useState([])
 
-                    </div>
-                </section>
-            </div>
+    useEffect(() => {
+        setcontentLoaded(false)
+        loadPhotos()
+    }, [])
+
+    async function loadPhotos() {
+        const query = '*[_type == "task"] {dailytask}'
+        const result = await client.fetch(query)
+        const baseUrl = urlFor(result[0]["dailytask"]["asset"]["_ref"].slice(6,))["options"].baseUrl
+        const dataset = urlFor(result[0]["dailytask"]["asset"]["_ref"].slice(6,))["options"].dataset
+        const projectId = urlFor(result[0]["dailytask"]["asset"]["_ref"].slice(6,))["options"].projectId
+
+        const items = await Promise.all(
+            result.map(async (i) => {
+                let item = {
+                    url:
+                        `${baseUrl}/images/${projectId}/${dataset}/${i["dailytask"]["asset"]["_ref"]
+                            .slice(6,).slice(0, -4) + '.'}png`
+
+                };
+                return item;
+            })
         )
+        setPhotos(items)
+        setcontentLoaded(true)
     }
+
+    return (
+        <div>
+            <BreadCrumb imageURL="/asssets/images/bg_2.jpg" pagename="Galleries" pageURL="GALLERY" />
+            <section class="ftco-section ftco-gallery">
+                <div class="container" >
+                    {!contentLoaded &&
+                    <div class='loader'>
+                        <Dna
+                            visible={true}
+                            height="150"
+                            width="150"
+                            ariaLabel="dna-loading"
+                            wrapperStyle={{}}
+                            wrapperClass="dna-wrapper"
+                        />
+                        </div>
+                    }
+                    {contentLoaded &&
+                        <div class="d-md-flex">
+                            {photos.map((photo, i) =>
+                                <Fancybox options={{ infinite: false }}>
+                                    <button class="gallery image-popup d-flex justify-content-center align-items-center img" data-fancybox="gallery"
+                                        style={{ backgroundImage: `url(${photo.url})` }}
+                                        data-src={photo.url}>
+                                        <div class="icon d-flex justify-content-center align-items-center">
+                                            <span class="icon-search"></span>
+                                        </div>
+                                    </button>
+                                </Fancybox>
+                            )}
+                        </div>
+                    }
+                </div>
+            </section>
+        </div>
+    )
 }
+export default Gallery;
