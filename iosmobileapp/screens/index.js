9bb26662e84239f6b5204a1148aa63554dee946e
eliashezron <eliashezron23@gmail.com>
2022-10-17 12:37:55 +0300

    restart

--- iosmobileapp/screens/index.js
+++
@@ -1,9 +0,0 @@
-import Home from "./Home"
-import Scan from "./Scan"
-import SignUp from "./SignUp"
-
-export {
-    Home,
-    Scan,
-    SignUp
-};
--- pesapay/mobileApp/constants/index.js
+++
@@ -1,5 +0,0 @@
-import icons from "./icons";
-import images from "./images";
-import theme, { COLORS, SIZES, FONTS } from "./theme";
-
-export { icons, images, theme, COLORS, SIZES, FONTS };
