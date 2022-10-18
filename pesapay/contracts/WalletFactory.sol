f79ad8384cb4d4cbf129c33b8ace344d3a3d8c0a
eliashezron <eliashezron23@gmail.com>
2022-10-18 11:22:07 +0300

    walletFactoy contracts completed

---
+++ pesapay/contracts/WalletFactory.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
+import "./WalletProxy.sol";
+import "hardhat/console.sol";
+
+contract WalletFactory is
+    Initializable,
+    OwnableUpgradeable,
+    PausableUpgradeable,
+    UUPSUpgradeable
+{
+    address public walletImplementation;
+
+    mapping(string => address) public wallets;
+
+    event NewWallet(string uuid, address indexed wallet);
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize(address _walletImp) public initializer {
+        __Ownable_init();
+        __Pausable_init();
+        walletImplementation = _walletImp;
+    }
+
+    function updateImplementation(address _walletImp)
+        public
+        onlyOwner
+        whenPaused
+    {
+        walletImplementation = _walletImp;
+    }
+
+    function newWallet(string memory userId) public returns (bool) {
+        require(bytes(userId).length >= 32, "WI: Invalid ID"); //If the passed string is ASCII character i.e 1 byte/character
+
+        address _wallet = address(new WalletProxy(address(this)));
+
+        wallets[userId] = _wallet;
+
+        emit NewWallet(userId, _wallet);
+
+        return true;
+    }
+
+    function _authorizeUpgrade(address) internal override onlyOwner {}
+}
--- pesapay/hardhat.config.js
+++ pesapay/hardhat.config.js
@@ -3,7 +3,7 @@ require("@openzeppelin/hardhat-upgrades")
 require("@nomiclabs/hardhat-ethers")
 require("@nomiclabs/hardhat-etherscan")
 require("hardhat-celo")
-require("dotenv").config()
+require("dotenv").config({ path: ".env" })
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -28,24 +28,6 @@ module.exports = {
       alfajores: process.env.CELOSCAN_API_KEY,
       celo: process.env.CELOSCAN_API_KEY,
     },
-    // customChains: [
-    //   {
-    //     network: "alfajores",
-    //     chainId: 44787,
-    //     urls: {
-    //       apiURL: "https://api-alfajores.celoscan.io/api",
-    //       browserURL: "https://alfajores.celoscan.io",
-    //     },
-    //   },
-    //   {
-    //     network: "celo",
-    //     chainId: 42220,
-    //     urls: {
-    //       apiURL: "https://api.celoscan.io/api",
-    //       browserURL: "https://celoscan.io/",
-    //     },
-    //   },
-    // ],
   },
   networks: {
     alfajores: {
@@ -58,5 +40,61 @@ module.exports = {
       accounts: [process.env.PRIVATE_KEY],
       chainId: 42220,
     },
+    mainnet: {
+      url: "https://mainnet.infura.io/v3/460f40a260564ac4a4f4b3fffb032dad",
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    goerli: {
+      url: "https://goerli.infura.io/v3/460f40a260564ac4a4f4b3fffb032dad",
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    bscTestnet: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      chainId: 97,
+      gasPrice: 20000000000,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    bsc: {
+      url: "https://bsc-dataseed.binance.org/",
+      chainId: 56,
+      gasPrice: 20000000000,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    polygon: {
+      url: "https://polygon-rpc.com",
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    fantom: {
+      url: "https://rpcapi.fantom.network",
+      gasPrice: 1000000000,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    testnetFantom: {
+      url: "https://rpc.testnet.fantom.network",
+      gasPrice: 1000000000,
+      accounts: [process.env.PRIVATE_KEY],
+    },
   },
 }
+// customChains: [
+//   {
+//     network: "alfajores",
+//     chainId: 44787,
+//     urls: {
+//       apiURL: "https://api-alfajores.celoscan.io/api",
+//       browserURL: "https://alfajores.celoscan.io",
+//     },
+//   },
+//   {
+//     network: "celo",
+//     chainId: 42220,
+//     urls: {
+//       apiURL: "https://api.celoscan.io/api",
+//       browserURL: "https://celoscan.io/",
+//     },
+//   },
+// ],
