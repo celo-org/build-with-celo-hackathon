2efabc0b834535629c428de3e44469a9db5669f8
Giftea <eleojogiftuhiene@gmail.com>
2022-10-18 13:19:15 +0100

    Added connect wallet functionality

--- src/packages/react-app/components_/Layout/Footer/index.tsx
+++ src/packages/react-app/components_/Layout/Footer/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import { footerlinks } from "@/data";
-import { Stack, Typography, Box, Grid } from "@mui/material";
+import { Stack, Typography, Grid } from "@mui/material";
 
 const Footer = () => {
   return (
--- src/packages/react-app/components_/Layout/Header/HeaderOne.tsx
+++ src/packages/react-app/components_/Layout/Header/HeaderOne.tsx
@@ -1,12 +1,38 @@
-import { Stack, Typography } from "@mui/material";
+import { Typography } from "@mui/material";
 import React from "react";
 import Section from "../Section";
+import { useCelo } from "@celo/react-celo";
+import { truncateAddress } from "@/utils";
+import {Chip} from "@mui/material";
+
 
 const HeaderOne = () => {
+  const { address, connect, destroy } = useCelo();
+
   return (
     <Section>
       <Typography variant="body2" className="text-sm">Buy $SAVEH token on SwapScanner</Typography>
-      <button className="btn-primary">Connect Wallet</button>
+      <>
+      {address && (
+        <>
+          <Chip
+            label={truncateAddress(address)}
+            variant="outlined"
+            onDelete={destroy}
+            sx={{ mx: 1 }}
+          />
+        </>
+      )}
+      {!address && (
+        <button
+          className="btn-primary"
+          onClick={() => connect().catch(e => console.log(e))}
+        >
+          Connect wallet
+        </button>
+      )}
+
+    </>
     </Section>
   );
 };
