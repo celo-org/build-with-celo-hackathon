95baee9274857e9c790160920775bc387bd4fb38
Kachi <kachdekan@gmail.com>
2022-10-21 15:29:35 +0300

    fix index conflict

--- CLIXPESA/features/spaces/index.js
+++ CLIXPESA/features/spaces/index.js
@@ -3,17 +3,9 @@ export { default as RoscaHomeScreen } from './RoscaHomeScreen'
 export { default as SelectContactsScreen } from './SelectContactsScreen'
 export { default as CustomizeGroupScreen } from './CustomizeGroupScreen'
 export { default as SetRoscaGoalScreen } from './SetRoscaGoalScreen'
-<<<<<<< HEAD
-<<<<<<< HEAD
-export { default as FundSpaceScreen } from './FundSpaceScreen'
+export { default as FundRoundScreen } from './FundRoundScreen'
 export { default as CustomizePersonalScreen } from './CustomizePersonalScreen'
 export { default as SetPersonalGoalScreen } from './SetPersonalGoalScreen'
 export { default as PersonalHomeScreen } from './PersonalHomeScreen'
 export { default as AddPersonalFundScreen } from './AddPersonalFundScreen'
 export { default as WithdrawFundScreen } from './WithdrawFundScreen'
-=======
-export { default as FundRoundScreen } from './FundRoundScreen'
->>>>>>> configure hardhart
-=======
-export { default as FundRoundScreen } from './FundRoundScreen'
->>>>>>> c235832250d5e17afceaed8cfcf91903f6177ce6
