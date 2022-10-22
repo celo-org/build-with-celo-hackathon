9dc0b57cc1465263a4f3b31dde28962fc41ce655
auma2022 <dianaauma2@gmail.com>
2022-10-22 13:39:11 +0300

    add home page

---
+++ Crypto_Ventures/frontend/pages/funding.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Funding() {
+    return (
+            
+    <div>
+      <h1>mail</h1>
+    </div>
+
+        
+    )
+}
+
+export default Funding;
---
+++ Crypto_Ventures/frontend/pages/login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Hero from '../components/Hero'
+
+function login() {
+  return (
+    <div>
+      <Hero title='Login' />
+    </div>
+  )
+}
+
+export default login
