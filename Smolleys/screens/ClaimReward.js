fbd887bc781267344a4948fa52739f3ebd739f1c
Fabrice Hafashimana <tumbafabruce@gmail.com>
2022-10-23 16:21:15 +0200

    Feat: The child is able to claim their reward
    - Added the claim screen

---
+++ Smolleys/screens/ClaimReward.js
@@ -0,0 +1,169 @@
+import { useRef, useEffect, useState } from "react";
+import { useIsFocused } from "@react-navigation/native";
+import {
+  Dimensions,
+  Image,
+  ImageBackground,
+  ScrollView,
+  ScrollViewBase,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import BottomNav from "../components/bottomNav";
+import IslandComponent from "../components/island";
+import TopBar from "../components/topBar";
+
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/storage";
+import { TouchableOpacity } from "react-native-gesture-handler";
+
+const win = Dimensions.get("window");
+
+const ClaimReward = ({ navigation }) => {
+  const isFocused = useIsFocused();
+  const scrollViewRef = useRef();
+  var snp = {};
+  const [tasks, setTasks] = useState({});
+  const [snnap, setSnnap] = useState({});
+  const [tmp, setTmp] = useState({});
+  const taskKeys = Object.keys(tasks);
+
+  useEffect(() => {
+    firebase
+      .firestore()
+      .collection("users")
+      .doc(firebase.auth().currentUser.uid)
+      .collection("tasks")
+      .orderBy("creation", "asc")
+      .get()
+      .then((snap) => {
+        // snp = snap.docs[0].id;
+        const items = {};
+        var i = 0;
+        snap.forEach(fetchTasks);
+        function fetchTasks(item) {
+          items[item.id] = item.data();
+          snp[item.id] = snap.docs[i].id;
+          i++;
+        }
+        setTasks(items);
+        setSnnap(snp);
+        console.log(snp);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [isFocused]);
+
+  return (
+    <ImageBackground
+      style={styles.bg}
+      source={require("../assets/mini-component-assets/OrangeBg.png")}
+    >
+      <View>
+        <Image
+          style={{ margin: 20 }}
+          source={require("../assets/mini-component-assets/back.png")}
+        />
+      </View>
+      <View
+        style={[
+          {
+            justifyContent: "center",
+            flexDirection: "row",
+            alignItems: "center",
+            color: "green",
+          },
+        ]}
+      >
+        <Text
+          style={[
+            styles.todoText,
+            { color: "green", fontSize: 30, flexDirection: "column" },
+          ]}
+        >
+          <>Claim</> <>Rewards</>
+        </Text>
+      </View>
+      <ScrollView
+        contentContainerStyle={{
+          justifyContent: "center",
+          alignItems: "center",
+          height: win.height / 1.5,
+        }}
+      >
+        <ImageBackground
+          style={[styles.islandPop]}
+          source={require("../assets/mini-component-assets/moneyButton.png")}
+        >
+          <Image
+            source={require("../assets/mini-component-assets/moneyBig.png")}
+          />
+          <Text style={[styles.todoText, { color: "green" }]}>cUSD</Text>
+          <View style={{ marginTop: 15 }}></View>
+          <Text style={[styles.todoText, { color: "green" }]}>
+            Total Balance: $40
+          </Text>
+          <TouchableOpacity onPress={() => navigation.navigate("Rewards")}>
+            <ImageBackground
+              source={require("../assets/mini-component-assets/blueButton.png")}
+              style={styles.button}
+            >
+              <Text style={styles.todoText}>Claim</Text>
+            </ImageBackground>
+          </TouchableOpacity>
+        </ImageBackground>
+      </ScrollView>
+      <BottomNav navigation={navigation} />
+    </ImageBackground>
+  );
+};
+
+export default ClaimReward;
+
+const styles = StyleSheet.create({
+  bg: {
+    height: win.height + 40,
+    flexDirection: "column",
+  },
+  button: {
+    height: 70,
+    padding: 10,
+    marginBottom: 0,
+    elevation: 11,
+    zIndex: 11,
+    width: win.width - 192,
+    resizeMode: "contain",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  islandPop: {
+    // position: "absolute",
+    // top: win.height / 3,
+    //left: win.width / 10,
+    elevation: 10,
+    zIndex: 10,
+    width: 270,
+    margin: 10,
+    // resizeMode: "contain",
+    height: 210,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  todoText: {
+    fontSize: 20,
+    color: "white",
+    fontFamily: "BubblegumSans_400Regular",
+  },
+  direction: {
+    // flexDirection: "column-reverse",
+
+    height: win.height + 800,
+    // paddingBottom: 200,
+    flexDirection: "column-reverse",
+    justifyContent: "flex-start",
+    // paddingBottom: win.height / 6,
+  },
+});
---
+++ Smolleys/screens/TimelineScreen.js
@@ -0,0 +1,95 @@
+import {
+  Text,
+  StyleSheet,
+  View,
+  Dimensions,
+  ScrollView,
+  Image,
+} from "react-native";
+import React, { Component, useState, useEffect } from "react";
+import BottomNav from "../components/bottomNav";
+import Timeline from "../components/timeline";
+const win = Dimensions.get("window");
+
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/storage";
+
+const TimelineScreen = ({ navigation }) => {
+  const [posts, setPosts] = useState({});
+  const postKeys = Object.keys(posts);
+
+  useEffect(() => {
+    firebase
+      .firestore()
+      .collection("post")
+      .doc(firebase.auth().currentUser.uid)
+      .collection("userPosts")
+      .orderBy("creation", "desc")
+      .get()
+      .then((snap) => {
+        const items = {};
+        snap.forEach((item) => {
+          items[item.id] = item.data();
+        });
+        setPosts(items);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <View style={styles.bg}>
+      <Image
+        style={[styles.bar, styles.barPosition, styles.float]}
+        source={require("../assets/mini-component-assets/profilexl.png")}
+      />
+      <ScrollView>
+        <View style={styles.pdg}>
+          {postKeys.length > 0 ? (
+            postKeys.map((key) => (
+              <Timeline
+                key={key}
+                id={key}
+                value={posts[key]}
+                name="Edidiong Udoh"
+                src={posts[key].downloadURL}
+              />
+            ))
+          ) : (
+            <Text>No Post Yet</Text>
+          )}
+        </View>
+      </ScrollView>
+      <BottomNav navigation={navigation} />
+    </View>
+  );
+};
+export default TimelineScreen;
+
+const styles = StyleSheet.create({
+  bg: {
+    height: win.height + 40,
+  },
+  pdg: {
+    paddingTop: 80,
+  },
+  barPosition: {
+    top: 20,
+    right: 20,
+  },
+  bar: {
+    // width: win.width,
+    height: 80,
+    flexDirection: "row",
+    alignItems: "flex-end",
+    justifyContent: "flex-end",
+    alignSelf: "center",
+  },
+  float: {
+    position: "absolute",
+    elevation: 6,
+    zIndex: 6,
+  },
+});
