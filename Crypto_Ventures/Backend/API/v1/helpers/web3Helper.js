4369e52fc47f3ee09315c44f773e89fdfe5ddfa4
oryankibandi <shreckoryan@gmail.com>
2022-10-23 23:21:53 +0300

    added Web3 class

--- Crypto_Ventures/Backend/API/v1/helpers/web3Helper.js
+++ Crypto_Ventures/Backend/API/v1/helpers/web3Helper.js
@@ -1,9 +1,12 @@
 require("dotenv").config();
 const Web3 = require("web3");
+const Event = require("node:events");
 
-class Web3JS {
-  constructor() {
-    this.web3 = new Web3(process.env.CELO_ALFAJORES);
+class Web3JS extends Event {
+  constructor(network, contractABI, contractAddress) {
+    super();
+    this.web3 = new Web3(new Web3.providers.HttpProvider(network));
+    this.contract = new this.web3.eth.Contract(contractABI, contractAddress);
   }
 
   switchProvider(providerUrl) {
@@ -22,8 +25,76 @@ class Web3JS {
     return web3.eth.accounts.encrypt(privateKey, password);
   }
 
-  decrypto(keystoreJsonV3, password) {
+  decrypt(keystoreJsonV3, password) {
     return web3.eth.accounts.decrypt(keystoreJsonV3, password);
+  }
+
+  async createFund(nameOfFund, size, sender) {
+    const sizeInWei = this.toWei(size);
+    await this.contract.methods
+      .createFund(nameOfFund, sizeInWei)
+      .send({
+        from: sender,
+      })
+      .on("confirmation", (confirmationNumber, receipt) => {
+        this.emit("fundCreated", { nameOfFund, size, sender });
+        console.log("confirmationNumber:", confirmationNumber);
+        console.log("receipt:", receipt);
+      })
+      .on("error", (error, receipt) => console.log("ERROR:", error));
+  }
+
+  async investInFund(fundOwnerAddress, investorAddress, nameOfFund, amount) {
+    const amountInWei = this.toWei(amount);
+    await this.contract.methods
+      .investInFund(fundOwnerAddress, nameOfFund)
+      .send({
+        from: investorAddress,
+        value: amountInWei,
+      })
+      .on("receipt", () =>
+        this.emit("investedInFund", { nameOfFund, amount, investorAddress })
+      )
+      .on("error", (error, receipt) => console.log("ERROR:", error));
+  }
+
+  async investInStartup(vcFirmAddress, startupAddress, nameOfFund, size) {
+    const sizeInWei = this.toWei(size);
+    await this.contract.methods
+      .investInStartup(startupAddress, nameOfFund)
+      .send({
+        from: vcFirmAddress,
+        value: sizeInWei,
+      })
+      .on("transactionHash", (hash) => {
+        this.emit("investedInStartup", { startupAddress, nameOfFund, size });
+        console.log("hash:", hash);
+      });
+  }
+
+  async getTotalContributions(nameOfFund) {
+    const balance = await this.contract.methods
+      .getTotContributions(nameOfFund)
+      .call()
+      .then((res) => {
+        return res;
+      });
+
+    return this.toEther(balance);
+  }
+
+  /**
+   * `toWei` - converts ether to Wei
+   * @param {Number} ether
+   *
+   * @returns Wei equivalent of the passed value
+   */
+  toWei(ether) {
+    return this.web3.utils.toWei(ether, "ether");
+  }
+
+  toEther(wei) {
+    return this.web3.utils.fromWei(wei, "ether");
   }
 }
 
