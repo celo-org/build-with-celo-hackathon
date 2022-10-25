0f3e32e2a4537aa7fea7cd88453306174bca86b3
Caleb I. Lucas <caleb@lucasbin.com>
2022-10-26 00:16:12 +0200

    feat: update repo to directly query toucan subgraph

--- koral-earth/app/backend/market/market.repo.ts
+++ koral-earth/app/backend/market/market.repo.ts
@@ -1,6 +1,32 @@
 import { Project } from './market.entity';
 import toucanProjects from '../shared/data/toucanProjects.json';
+import axios from 'axios';
 
 export const getProjects = async (): Promise<Project[]> => {
-  return toucanProjects.data.projects;
+  try {
+    const results = await axios.post<typeof toucanProjects>(
+      'https://api.thegraph.com/subgraphs/name/toucanprotocol/matic',
+      {
+        query: `{
+        projects(where: {}) {
+          region
+          standard
+          methodology
+          projectId
+          vintages {
+            id
+          }
+        }
+      }`,
+        variables: null,
+      }
+    );
+    return results.data.data.projects;
+  } catch (error) {
+    console.log(
+      'Getting projects failed! Defaulting to pre-fetched projects',
+      error
+    );
+    return toucanProjects.data.projects;
+  }
 };
