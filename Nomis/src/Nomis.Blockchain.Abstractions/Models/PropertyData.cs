674fe8492a92baf91f967092fd86e5abb826934b
Chebotov Nickolay <spiritkola@hotmail.com>
2022-10-26 14:12:52 +0300

    update: Update CeloWalletStats

---
+++ Nomis/src/Nomis.Blockchain.Abstractions/Models/PropertyData.cs
@@ -0,0 +1,61 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+using Humanizer;
+
+namespace Nomis.Blockchain.Abstractions.Models
+{
+    /// <summary>
+    /// Property data.
+    /// </summary>
+    public class PropertyData
+    {
+        /// <summary>
+        /// Initialize <see cref="PropertyData"/>.
+        /// </summary>
+        /// <param name="propertyInfo"><see cref="PropertyInfo"/>.</param>
+        public PropertyData(
+            PropertyInfo propertyInfo)
+        {
+            Label = propertyInfo.GetCustomAttribute<DisplayAttribute>()?.Name
+                    ?? propertyInfo.Name.Humanize();
+            Description = propertyInfo.GetCustomAttribute<DisplayAttribute>()?.Description
+                          ?? propertyInfo.GetCustomAttribute<DescriptionAttribute>()?.Description
+                          ?? propertyInfo.Name.Humanize();
+            Units = propertyInfo.GetCustomAttribute<DisplayAttribute>()?.GroupName
+                    ?? "unknown";
+        }
+
+        /// <summary>
+        /// Initialize <see cref="PropertyData"/>.
+        /// </summary>
+        /// <param name="label">Property label.</param>
+        /// <param name="description">Property description.</param>
+        /// <param name="units">Units.</param>
+        public PropertyData(
+            string label,
+            string description,
+            string units)
+        {
+            Label = label;
+            Description = description;
+            Units = units;
+        }
+
+        /// <summary>
+        /// Property label.
+        /// </summary>
+        public string? Label { get; }
+
+        /// <summary>
+        /// Property description.
+        /// </summary>
+        public string? Description { get; }
+
+        /// <summary>
+        /// Units.
+        /// </summary>
+        public string? Units { get; }
+    }
+}
--- Nomis/src/Nomis.Celoscan.Interfaces/Models/CeloWalletStats.cs
+++ Nomis/src/Nomis.Celoscan.Interfaces/Models/CeloWalletStats.cs
@@ -1,6 +1,7 @@
-﻿using System.ComponentModel;
+﻿using System.ComponentModel.DataAnnotations;
 
 using Nomis.Blockchain.Abstractions;
+using Nomis.Blockchain.Abstractions.Models;
 
 namespace Nomis.Celoscan.Interfaces.Models
 {
@@ -8,7 +9,7 @@ namespace Nomis.Celoscan.Interfaces.Models
     /// Celo wallet stats.
     /// </summary>
     public class CeloWalletStats
-        : IWalletStats
+        : IWalletStats<CeloTransactionIntervalData>
     {
         /// <summary>
         /// No data.
@@ -16,79 +17,95 @@ namespace Nomis.Celoscan.Interfaces.Models
         public bool NoData { get; init; }
 
         /// <summary>
-        /// Amount of deployed smart-contracts (number).
+        /// Amount of deployed smart-contracts.
         /// </summary>
-        [Description("Amount of deployed smart-contracts (number)")]
+        [Display(Description = "Amount of deployed smart-contracts", GroupName = "number")]
         public int DeployedContracts { get; init; }
 
         /// <inheritdoc/>
-        [Description("Wallet balance (CELO)")]
+        [Display(Description = "Wallet balance", GroupName = "CELO")]
         public decimal Balance { get; set; }
 
         /// <inheritdoc/>
-        [Description("Wallet age (months)")]
+        [Display(Description = "Wallet balance", GroupName = "USD")]
+        public decimal BalanceUSD { get; set; }
+
+        /// <inheritdoc/>
+        [Display(Description = "Wallet age", GroupName = "months")]
         public int WalletAge { get; set; }
 
         /// <inheritdoc/>
-        [Description("Total transactions on wallet (number)")]
+        [Display(Description = "Total transactions on wallet", GroupName = "number")]
         public int TotalTransactions { get; set; }
 
         /// <inheritdoc/>
-        [Description("Total rejected transactions on wallet (number)")]
+        [Display(Description = "Total rejected transactions on wallet", GroupName = "number")]
         public int TotalRejectedTransactions { get; set; }
 
         /// <inheritdoc/>
-        [Description("Average time interval between transactions (hours)")]
+        [Display(Description = "Average time interval between transactions", GroupName = "hours")]
         public double AverageTransactionTime { get; set; }
 
         /// <inheritdoc/>
-        [Description("Maximum time interval between transactions (hours)")]
+        [Display(Description = "Maximum time interval between transactions", GroupName = "hours")]
         public double MaxTransactionTime { get; set; }
 
         /// <inheritdoc/>
-        [Description("Minimal time interval between transactions (hours)")]
+        [Display(Description = "Minimal time interval between transactions", GroupName = "hours")]
         public double MinTransactionTime { get; set; }
 
         /// <inheritdoc/>
-        [Description("The movement of funds on the wallet (CELO)")]
+        [Display(Description = "The movement of funds on the wallet", GroupName = "CELO")]
         public decimal WalletTurnover { get; set; }
 
-        /// <summary>
-        /// The intervals of funds movements on the wallet.
-        /// </summary>
-        [Description("The intervals of funds movements on the wallet")]
+        /// <inheritdoc/>
+        [Display(Description = "The intervals of funds movements on the wallet", GroupName = "collection")]
         public IEnumerable<CeloTransactionIntervalData>? TurnoverIntervals { get; set; }
 
         /// <inheritdoc/>
-        [Description("Total NFTs on wallet (number)")]
+        [Display(Description = "The balance change value in the last month", GroupName = "CELO")]
+        public decimal BalanceChangeInLastMonth { get; set; }
+
+        /// <inheritdoc/>
+        [Display(Description = "The balance change value in the last year", GroupName = "CELO")]
+        public decimal BalanceChangeInLastYear { get; set; }
+
+        /// <inheritdoc/>
+        [Display(Description = "Total NFTs on wallet", GroupName = "number")]
         public int NftHolding { get; set; }
 
         /// <inheritdoc/>
-        [Description("Time since last transaction (months)")]
+        [Display(Description = "Time since last transaction", GroupName = "months")]
         public int TimeFromLastTransaction { get; set; }
 
         /// <inheritdoc/>
-        [Description("NFT trading activity (CELO)")]
+        [Display(Description = "NFT trading activity", GroupName = "CELO")]
         public decimal NftTrading { get; set; }
 
         /// <inheritdoc/>
-        [Description("NFT worth on wallet (CELO)")]
+        [Display(Description = "NFT worth on wallet", GroupName = "CELO")]
         public decimal NftWorth { get; set; }
 
         /// <inheritdoc/>
-        [Description("Last month transactions (number)")]
+        [Display(Description = "Last month transactions", GroupName = "number")]
         public int LastMonthTransactions { get; set; }
 
         /// <inheritdoc/>
-        [Description("Average transaction per months (number)")]
-        public double TransactionsPerMonth => WalletAge != 0 ? (double)TotalTransactions / WalletAge : 0;
-
-        /// <inheritdoc/>
-        [Description("Last year transactions on wallet (number)")]
+        [Display(Description = "Last year transactions on wallet", GroupName = "number")]
         public int LastYearTransactions { get; set; }
 
         /// <inheritdoc/>
-        [Description("Value of all holding tokens (number)")]
+        [Display(Description = "Average transaction per months", GroupName = "number")]
+        public double TransactionsPerMonth => WalletAge != 0 ? (double)TotalTransactions / WalletAge : 0;
+
+        /// <inheritdoc/>
+        [Display(Description = "Value of all holding tokens", GroupName = "number")]
         public int TokensHolding { get; set; }
+
+        /// <inheritdoc/>
+        public Dictionary<string, PropertyData> StatsDescriptions => GetType()
+            .GetProperties()
+            .Where(prop => Attribute.IsDefined(prop, typeof(DisplayAttribute)))
+            .ToDictionary(p => p.Name, p => new PropertyData(p));
     }
 }
