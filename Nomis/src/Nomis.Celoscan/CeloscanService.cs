d5507e951c00f71ced5c815f330ce36390b6619a
Chebotov Nickolay <spiritkola@hotmail.com>
2022-10-26 16:08:44 +0300

    update(CeloscanService): Implement GetWalletStatsAsync method

--- Nomis/src/Nomis.Celoscan/CeloscanService.cs
+++ Nomis/src/Nomis.Celoscan/CeloscanService.cs
@@ -1,6 +1,11 @@
-﻿using Nomis.Celoscan.Interfaces;
+﻿using System.Net;
+
+using Nethereum.Util;
+using Nomis.Celoscan.Calculators;
+using Nomis.Celoscan.Interfaces;
 using Nomis.Celoscan.Interfaces.Models;
 using Nomis.Utils.Contracts.Services;
+using Nomis.Utils.Exceptions;
 using Nomis.Utils.Wrapper;
 
 namespace Nomis.Celoscan
@@ -26,9 +31,35 @@ namespace Nomis.Celoscan
         /// <inheritdoc/>
         public async Task<Result<CeloWalletScore>> GetWalletStatsAsync(string address)
         {
-            // TODO - implement
+            if (!new AddressUtil().IsValidAddressLength(address) || !new AddressUtil().IsValidEthereumAddressHexFormat(address))
+            {
+                throw new CustomException("Invalid address", statusCode: HttpStatusCode.BadRequest);
+            }
 
-            return new();
+            var balanceWei = (await Client.GetBalanceAsync(address)).Balance;
+            var usdBalance = 0; // TODO
+            var transactions = (await Client.GetTransactionsAsync<CeloscanAccountNormalTransactions, CeloscanAccountNormalTransaction>(address)).ToList();
+            var internalTransactions = (await Client.GetTransactionsAsync<CeloscanAccountInternalTransactions, CeloscanAccountInternalTransaction>(address)).ToList();
+            var erc20Tokens = (await Client.GetTransactionsAsync<CeloscanAccountERC20TokenEvents, CeloscanAccountERC20TokenEvent>(address)).ToList();
+            var tokens = (await Client.GetTransactionsAsync<CeloscanAccountERC721TokenEvents, CeloscanAccountERC721TokenEvent>(address)).ToList();
+
+            var walletStats = new CeloStatCalculator(
+                    address,
+                    decimal.TryParse(balanceWei, out var wei) ? wei : 0,
+                    usdBalance,
+                    transactions,
+                    internalTransactions,
+                    tokens,
+                    erc20Tokens)
+                .GetStats();
+
+            var score = walletStats.GetScore();
+            return await Result<CeloWalletScore>.SuccessAsync(new()
+            {
+                Address = address,
+                Stats = walletStats,
+                Score = score
+            }, "Got celo wallet score.");
         }
     }
 }
