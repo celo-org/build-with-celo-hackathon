cb8f4fc53712a25ce97a2b4e40a23c1c60ea860a
7i7o <mparij@gmail.com>
2022-10-26 15:51:54 -0300

    fixed unit tests

--- contracts/test/Sacuda.t.sol
+++ contracts/test/Sacuda.t.sol
@@ -1,23 +1,43 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.11;
 
+// import "forge-std/console.sol";
 import "forge-std/Test.sol";
 import "../src/Sacuda.sol";
 
 contract SacudaTest is Test {
+    address amy = vm.addr(1);
+    address betty = vm.addr(2);
+    address caro = vm.addr(3);
+    bytes32 admin;
+    bytes32 minter;
+
     Sacuda public sacuda;
+
     function setUp() public {
-       sacuda = new Sacuda();
-       sacuda.setNumber(0);
+        sacuda = new Sacuda();
+        minter = sacuda.MINTER_ROLE();
+        admin = sacuda.ADMIN_ROLE();
     }
 
-    function testIncrement() public {
-        sacuda.increment();
-        assertEq(sacuda.number(), 1);
+    function testMint() public {
+        sacuda.grantRole(minter, caro);
+        vm.startPrank(caro);
+        sacuda.mint(amy, "Amy");
+        assertEq(sacuda.ownerOf(1), amy);
+        assertEq(sacuda.name(1), "Amy");
     }
 
-    function testSetNumber(uint256 x) public {
-        sacuda.setNumber(x);
-        assertEq(sacuda.number(), x);
+    function testUpdateReport() public {
+        uint8 num = 75;
+        sacuda.grantRole(admin, caro);
+        sacuda.grantRole(minter, caro);
+        vm.startPrank(caro);
+        (uint8 paymentHistory, , , , ) = sacuda.report(2);
+        assertEq(paymentHistory, 0);
+        sacuda.mint(betty, "Betty");
+        sacuda.updateReport(2, abi.encode(num, num, num, num, num));
+        (paymentHistory, , , , ) = sacuda.report(2);
+        assertEq(paymentHistory, num);
     }
 }
