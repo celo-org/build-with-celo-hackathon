ed23d180f9c9fe13165c22262cb4821b9453c80d
Mystical94 <53875398+Mystical94@users.noreply.github.com>
2022-10-27 20:46:21 +0530

    default token list
    updated default token list

--- EviveSwap/UserInterface/src/constants/lists.ts
+++ EviveSwap/UserInterface/src/constants/lists.ts
@@ -1,20 +1,5 @@
-const list = 'https://raw.githubusercontent.com/Mystical94/build-with-celo-hackathon--EviveSwap/eviveswap/EviveSwap/UserInterface/src/constants/token/eviveswap.json'
-export const DEFAULT_ACTIVE_LIST_URLS: string[] = [list]
+const EviveSwap_List = 'https://raw.githubusercontent.com/Mystical94/build-with-celo-hackathon--EviveSwap/eviveswap/EviveSwap/UserInterface/src/constants/token/eviveswap.json'
 
-export const DEFAULT_LIST_OF_LISTS: string[] = [list]
+export const DEFAULT_ACTIVE_LIST_URLS: string[] = [EviveSwap_List]
 
-/*const COMPOUND_LIST = 'https://raw.githubusercontent.com/compound-finance/token-list/master/compound.tokenlist.json';
-const UBESWAP_LIST = 'https://raw.githubusercontent.com/Ubeswap/default-token-list/master/ubeswap.token-list.json'
-//const UBESWAP_EXPERIMENTAL_LIST =
-//  'https://raw.githubusercontent.com/Ubeswap/default-token-list/master/ubeswap-experimental.token-list.json'
-// lower index == higher priority for token import
-export const DEFAULT_LIST_OF_LISTS: string[] = [
-    UBESWAP_LIST,
-    //UBESWAP_EXPERIMENTAL_LIST,
-    COMPOUND_LIST,
-    //...UNSUPPORTED_LIST_URLS, // need to load unsupported tokens as well
-  ]
-
-// default lists to be 'active' aka searched across
-export const DEFAULT_ACTIVE_LIST_URLS: string[] = [UBESWAP_LIST, COMPOUND_LIST]
-*/
+export const DEFAULT_LIST_OF_LISTS: string[] = [EviveSwap_List]
