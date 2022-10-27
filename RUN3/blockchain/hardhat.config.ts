87a8db7632ed7112716cf2b94954d12ee54cec15
Jose Mendez <jose@qs-iMac.local>
2022-10-26 20:55:00 -0600

    use lavanet rpc node

--- RUN3/blockchain/hardhat.config.ts
+++ RUN3/blockchain/hardhat.config.ts
@@ -6,7 +6,7 @@ module.exports = {
     localhost: { url: `http://127.0.0.1:7545` },
     //Alfajores testnet
     testnet: {
-      url: 'https://alfajores-forno.celo-testnet.org',
+      url: 'https://celo-hackathon.lavanet.xyz/celo-alfajores/http',
       accounts: ['555bbf12d0931a8068f41c2126a1860f85727a4156e5e22bf224c1604dbb0919'],
     },
   },
