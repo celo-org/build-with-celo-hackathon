4e0767e39fbd35fce5b7bd6e369db661f9456683
Mystical94 <53875398+Mystical94@users.noreply.github.com>
2022-10-27 19:35:44 +0530

    default list

--- EviveSwap/UserInterface/src/constants/lists.ts
+++ EviveSwap/UserInterface/src/constants/lists.ts
@@ -1,6 +1,9 @@
-//export const UNSUPPORTED_LIST_URLS: string[] = []
+const list = 'https://raw.githubusercontent.com/Mystical94/build-with-celo-hackathon--EviveSwap/eviveswap/EviveSwap/UserInterface/src/constants/token/eviveswap.json'
+export const DEFAULT_ACTIVE_LIST_URLS: string[] = [list]
 
-const COMPOUND_LIST = 'https://raw.githubusercontent.com/compound-finance/token-list/master/compound.tokenlist.json';
+export const DEFAULT_LIST_OF_LISTS: string[] = [list]
+
+/*const COMPOUND_LIST = 'https://raw.githubusercontent.com/compound-finance/token-list/master/compound.tokenlist.json';
 const UBESWAP_LIST = 'https://raw.githubusercontent.com/Ubeswap/default-token-list/master/ubeswap.token-list.json'
 //const UBESWAP_EXPERIMENTAL_LIST =
 //  'https://raw.githubusercontent.com/Ubeswap/default-token-list/master/ubeswap-experimental.token-list.json'
@@ -14,3 +17,4 @@ export const DEFAULT_LIST_OF_LISTS: string[] = [
 
 // default lists to be 'active' aka searched across
 export const DEFAULT_ACTIVE_LIST_URLS: string[] = [UBESWAP_LIST, COMPOUND_LIST]
+*/
