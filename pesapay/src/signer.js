db1a0944b6a4831ee85171a1ede1bbecab81b35b
eliashezron <eliashezron23@gmail.com>
2022-10-27 08:56:43 +0300

    error handling

--- pesapay/src/signer.js
+++ pesapay/src/signer.js
@@ -23,7 +23,7 @@ function getMetaTxTypeData(chainId, verifyingContract) {
       ForwardRequest,
     },
     domain: {
-      name: "MinimalForwarder",
+      name: "MinimalForwarderUpgradeable",
       version: "0.0.1",
       chainId,
       verifyingContract,
