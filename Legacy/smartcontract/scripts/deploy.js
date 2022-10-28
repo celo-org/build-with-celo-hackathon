065e73bdfbace4b9ae2847c90cda419059371007
Hexdee <lukmansaheed79@gmail.com>
2022-10-28 11:15:46 +0100

    Write deploy script

--- Legacy/smartcontract/scripts/deploy.js
+++ Legacy/smartcontract/scripts/deploy.js
@@ -7,22 +7,16 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const Legacies = await hre.ethers.getContractFactory("Legacies");
+  const legacies = await Legacies.deploy();
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  await legacies.deployed();
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
+  
   console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+    `Legacy contract deployed to ${legacies.address}`
+    );
 }
-
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
