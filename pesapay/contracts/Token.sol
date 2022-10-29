4583fc06a51692a9c82aee6f0c4081aa386edcb1
eliashezron <eliashezron23@gmail.com>
2022-10-29 23:47:58 +0300

    test

--- pesapay/contracts/Token.sol
+++ pesapay/contracts/Token.sol
@@ -1,11 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+// import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@rari-capital/solmate/src/tokens/ERC20.sol";
 
 contract Token is ERC20, Ownable {
-    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
+    constructor(string memory name, string memory symbol)
+        ERC20(name, symbol, 18)
+    {}
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
