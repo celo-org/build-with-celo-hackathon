92556e8610661bbf9880465f228d8e5a02a032be
Chebotov Nickolay <spiritkola@hotmail.com>
2022-10-29 19:09:55 +0300

    update(Web): Add Web server project

---
+++ Nomis/src/Nomis.Web.Server.Common/Extensions/ConfigurationBuilderExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Nomis.Blockchain.Abstractions.Settings;
+using Nomis.Celoscan.Interfaces.Settings;
+using Nomis.Coingecko.Interfaces.Settings;
+
+namespace Nomis.Web.Server.Common.Extensions
+{
+    /// <summary>
+    /// <see cref="IConfigurationBuilder"/> extension methods.
+    /// </summary>
+    public static class ConfigurationBuilderExtensions
+    {
+        private const string ConfigsDirectory = "Configs";
+
+        /// <summary>
+        /// Add configuration from json.
+        /// </summary>
+        /// <param name="manager"><see cref="IConfigurationBuilder"/>.</param>
+        /// <returns>Returns <see cref="IConfigurationBuilder"/>.</returns>
+        public static IConfigurationBuilder AddJsonConfigs(this IConfigurationBuilder manager)
+        {
+            string env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production";
+
+            return manager
+                .AddJsonFile(Path.Combine(ConfigsDirectory, $"{nameof(ApiVisibilitySettings).ToLower()}.json"), false, true)
+                .AddJsonFile(Path.Combine(ConfigsDirectory, $"{nameof(ApiVisibilitySettings).ToLower()}.{env}.json"), true, true)
+                .AddJsonFile(Path.Combine(ConfigsDirectory, $"{nameof(CeloscanSettings).ToLower()}.json"), false, true)
+                .AddJsonFile(Path.Combine(ConfigsDirectory, $"{nameof(CeloscanSettings).ToLower()}.{env}.json"), true, true)
+                .AddJsonFile(Path.Combine(ConfigsDirectory, $"{nameof(CoingeckoSettings).ToLower()}.json"), false, true)
+                .AddJsonFile(Path.Combine(ConfigsDirectory, $"{nameof(CoingeckoSettings).ToLower()}.{env}.json"), true, true); ;
+        }
+    }
+}
---
+++ Nomis/src/Nomis.Web.Server.Common/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Nomis.Celoscan.Extensions;
+using Nomis.Coingecko.Extensions;
+
+namespace Nomis.Web.Server.Common.Extensions
+{
+    /// <summary>
+    /// <see cref="IServiceCollection"/> extension methods.
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Add blockchain services.
+        /// </summary>
+        /// <param name="services"><see cref="IServiceCollection"/>.</param>
+        /// <param name="configuration"><see cref="IConfiguration"/>.</param>
+        /// <returns>Returns <see cref="IServiceCollection"/>.</returns>
+        public static IServiceCollection AddBlockchainServices(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            return services
+                .AddCoingeckoService(configuration)
+                .AddCeloscanService(configuration);
+        }
+    }
+}
