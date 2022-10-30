f52e9fb2a1a7f5691216f0d1b2affc6deb8ad5ef
Chebotov Nickolay <spiritkola@hotmail.com>
2022-10-30 15:32:21 +0300

    fix(CeloscanService): Add getting USD price in scoring

--- Nomis/src/Nomis.Celoscan/CeloscanService.cs
+++ Nomis/src/Nomis.Celoscan/CeloscanService.cs
@@ -3,7 +3,10 @@
 using Nethereum.Util;
 using Nomis.Celoscan.Calculators;
 using Nomis.Celoscan.Interfaces;
+using Nomis.Celoscan.Interfaces.Extensions;
 using Nomis.Celoscan.Interfaces.Models;
+using Nomis.Celoscan.Responses;
+using Nomis.Coingecko.Interfaces;
 using Nomis.Utils.Contracts.Services;
 using Nomis.Utils.Exceptions;
 using Nomis.Utils.Wrapper;
@@ -15,13 +18,18 @@ namespace Nomis.Celoscan
         ICeloscanService,
         ITransientService
     {
+        private readonly ICoingeckoService _coingeckoService;
+
         /// <summary>
         /// Initialize <see cref="CeloscanService"/>.
         /// </summary>
         /// <param name="client"><see cref="ICeloscanClient"/>.</param>
+        /// <param name="coingeckoService"><see cref="ICoingeckoService"/>.</param>
         public CeloscanService(
-            ICeloscanClient client)
+            ICeloscanClient client,
+            ICoingeckoService coingeckoService)
         {
+            _coingeckoService = coingeckoService;
             Client = client;
         }
 
@@ -37,7 +45,7 @@ namespace Nomis.Celoscan
             }
 
             var balanceWei = (await Client.GetBalanceAsync(address)).Balance;
-            var usdBalance = 0; // TODO
+            var usdBalance = await _coingeckoService.GetUsdBalanceAsync<CoingeckoCeloUsdPriceResponse>(balanceWei?.ToCelo() ?? 0, "celo");
             var transactions = (await Client.GetTransactionsAsync<CeloscanAccountNormalTransactions, CeloscanAccountNormalTransaction>(address)).ToList();
             var internalTransactions = (await Client.GetTransactionsAsync<CeloscanAccountInternalTransactions, CeloscanAccountInternalTransaction>(address)).ToList();
             var erc20Tokens = (await Client.GetTransactionsAsync<CeloscanAccountERC20TokenEvents, CeloscanAccountERC20TokenEvent>(address)).ToList();
