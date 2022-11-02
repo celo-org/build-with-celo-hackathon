58c659176f3f80d57008065193e723a94347d59e
Ankush263 <ankushbanik263@gmail.com>
2022-11-02 12:40:23 +0530

    update

--- FOW FR/hardhat.config.ts
+++ FOW FR/hardhat.config.ts
@@ -1,8 +1,28 @@
 import { HardhatUserConfig } from "hardhat/config";
+require('dotenv').config();
 import "@nomicfoundation/hardhat-toolbox";
+
+const PRIVATE_KEY: any = process.env.PRIVATE_KEY
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks: {
+    alfajores: {
+      url: "https://alfajores-forno.celo-testnet.org",
+      accounts: [PRIVATE_KEY],
+      chainId: 44787,
+    },
+    alfajoresDatahub: {
+      url: "https://celo-alfajores--rpc.datahub.figment.io/apikey/<API KEY>",
+      accounts: [PRIVATE_KEY],
+      chainId: 44787,
+    },
+    celo: {
+      url: "https://forno.celo.org",
+      accounts: [PRIVATE_KEY],
+      chainId: 42220,
+    }
+  }
 };
 
 export default config;
