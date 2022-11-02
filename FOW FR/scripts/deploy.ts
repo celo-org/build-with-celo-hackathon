5dfc45638162ffff8739109ae0c0bc6359218783
Ankush263 <ankushbanik263@gmail.com>
2022-11-02 12:41:35 +0530

    update

--- FOW FR/scripts/deploy.ts
+++ FOW FR/scripts/deploy.ts
@@ -1,20 +1,9 @@
-import { ethers } from "hardhat";
+import ethers from "@nomiclabs/hardhat-ethers"
+const hre = require("hardhat")
 
 async function main() {
-  // const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  // const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  // const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
 
-  // const lockedAmount = ethers.utils.parseEther("1");
-
-  // const Lock = await ethers.getContractFactory("Lock");
-  // const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  // await lock.deployed();
-
-  // console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
-
-  const CELO = await ethers.getContractFactory("CELO_HACK")
+  const CELO = await hre.ethers.getContractFactory("CELO_HACK")
   const celo = await CELO.deploy()
 
   await celo.deployed()
@@ -23,9 +12,11 @@ async function main() {
 
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
+
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+
+
+//0x813B8a84A802aAdA2A873a8cbcAa703BEE9f68C4
