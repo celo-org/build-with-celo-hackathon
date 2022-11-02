11ece4dfcb07b83470515ee87e890952b7a75548
sandeep bhowmik <bhowmik.was.sandeep8@gmail.com>
2022-11-02 16:04:43 +0000

    Carousal in landing page

--- frontend/src/components/cause.js
+++ frontend/src/components/cause.js
@@ -8,7 +8,7 @@ const Cause = ({ details }) => {
             <span class="img" style={{ backgroundImage: `url(${details.campaignPic})` }} />
             <div class="text p-3 p-md-4">
                 <h3><a href="/campaign-details">{details.name}</a></h3>
-                <p class="desc-p">{details.description}</p>
+                <p class="description">{details.description}</p>
                 <div class="progress custom-progress-success">
                     <div class="progress-bar bg-primary" role="progressbar" style={{ width: "99%" }} aria-valuenow="99" aria-valuemin="0" aria-valuemax="100"></div>
                 </div>
