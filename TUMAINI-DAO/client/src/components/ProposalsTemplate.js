3b3e758ca4f9e49bfdb3a50c15fb9a3bd6189ac5
RonexLemon <ronexondimu@gmail.com>
2022-11-03 22:40:42 +0300

    modification

--- TUMAINI-DAO/client/src/components/ProposalsTemplate.js
+++ TUMAINI-DAO/client/src/components/ProposalsTemplate.js
@@ -40,7 +40,7 @@ const ProposalTemplate =(props)=>{
        </h4>
      </div>
      <div className="">
-       <button onClick={() => navigate1('/proposal_page', { state: { id: index } })} className="focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 text-sm font-bold leading-none text-white focus:outline-none bg-blue-500 border rounded hover:bg-blue-600 py-3 px-5 w-full font-open ">
+       <button onClick={() => navigate1('/proposal_page', { state: { id: index ,param:proposals[5]} })} className="focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 text-sm font-bold leading-none text-white focus:outline-none bg-blue-500 border rounded hover:bg-blue-600 py-3 px-5 w-full font-open ">
          View Proposal
        </button>
      </div>
