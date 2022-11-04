ad31f9672c21295d6b47f034a8e41b572a60c6ee
RonexLemon <ronexondimu@gmail.com>
2022-11-04 09:20:44 +0300

    implement comunity page

--- TUMAINI-DAO/client/src/components/Contribute.js
+++ TUMAINI-DAO/client/src/components/Contribute.js
@@ -1,7 +1,15 @@
 const Contribute = () => {
   return (
-    <div className="bg-indigo-50 p-3">
-      <div className="flex justify-around bg-white p-5">
+    <div className="grid grid-cols-2 divide-x m-1">
+      <div className=" bg-slate-300 h-60 rounded-2xl m-1 flex justify-start p-3">
+        <div>
+          <h2 className=" text-green-400 font-bold">cUSD Balance: </h2><br/>
+          <h2 className=" font-bold text-orange-400">Rehema  Balance: </h2>
+        </div>
+
+      </div>
+      <div className=" bg-slate-300 h-60 rounded-2xl m-1">
+      <div className="flex justify-around  p-5">
         <div>
           <h3 className="font-open text-4xl">
             Want to Contribute to the platform ?{" "}
@@ -10,20 +18,15 @@ const Contribute = () => {
         <div className="max-w-[500px] w-[100%]">
           <form>
             <div className="mt-2">
-              <h3 className="font-open">Enter Address Name</h3>
+              <h3 className="font-open">Enter Amount </h3>
               <input
                 type="text"
-                placeholder="name..."
+                placeholder="30..."
                 className="max-w-[800px] w-[100%] border-2 border-slate-800 p-2 rounded"
               />
             </div>
             <div className="mt-2">
-              <h3 className="font-open">Enter Address </h3>
-              <input
-                type="text"
-                placeholder="address..."
-                className="max-w-[800px] w-[100%] border-2 border-slate-800 p-2 rounded"
-              />
+             
               <input
                 className="focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 text-sm font-bold leading-none text-white focus:outline-none bg-blue-500 border rounded hover:bg-blue-600 py-3 px-5 w-[200px] font-open my-2"
                 type="submit"
@@ -33,6 +36,60 @@ const Contribute = () => {
           </form>
         </div>
       </div>
+        </div>
+        <div className="  bg-slate-300 h-60 rounded-2xl m-1">
+         <h1 className="flex  justify-center font-bold text-blue-400">Share Rehema Token</h1><br/>
+         <div className="flex justify-around">
+          <div className="font-bold justify-start prose-sm">
+            <h2>Enter Address and amount to share your Rehema Tokens</h2>
+          </div>
+          <div className="p-1 px-5">
+          <div className="mt-2">
+              <h3 className="font-open">Enter Amount </h3>
+              <input
+                type="text"
+                placeholder="30..."
+                className="max-w-[800px] w-[100%] border-2 border-slate-800 p-2 rounded"
+              />
+            </div>
+            <div className="mt-2 ">
+              <h3 className="font-open">Enter Address </h3>
+              <input
+                type="text"
+                placeholder="0xhjfjnjr..."
+                className="max-w-[800px] w-[100%] border-2 border-slate-800 p-2 rounded"
+              />
+            </div>
+            <input
+                className="focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 text-sm font-bold leading-none text-white focus:outline-none bg-blue-500 border rounded hover:bg-blue-600 py-1 px-5 w-[200px] font-open my-2"
+                type="submit"
+                name="Send Transcation"
+              />
+          </div>
+         </div>
+        </div>
+        <div className=" bg-slate-300 h-60 rounded-2xl m-1 px-5  ">
+        <h1 className="flex justify-center text-yellow-200">Buy Rehema Token</h1>
+        <div className="flex justify-around">
+        <div className="mt-2">
+              <h3 className="font-open">Enter Amount </h3>
+              <input
+                type="text"
+                placeholder="30..."
+                className="max-w-[200px] w-[100%] border-2 border-slate-800 p-2 rounded"
+              />
+              <input
+                className="focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 text-sm font-bold leading-none text-white focus:outline-none bg-blue-500 border rounded hover:bg-blue-600 py-1 px-5 w-[300px] h-8 font-open my-2"
+                type="submit"
+                name="Send Transcation"
+              />
+            </div>
+            
+
+        </div>
+        </div>
+
+      
     </div>
   );
 };
