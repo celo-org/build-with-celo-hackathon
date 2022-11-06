b054f345f65d688c64cbddf98448316692f10317
Denis Shutock <yoshutock@gmail.com>
2022-11-06 18:53:35 +0600

    Add Utilities .json Files

---
+++ Nomis/src/Nomis.Web.Client/utilities/menu.js
@@ -0,0 +1,33 @@
+export const mainMenu = [
+  {
+    title: "Docs",
+    link: "https://api.nomis.cc",
+  },
+  {
+    title: "Contact Us",
+    link: "mailto:gm@momis.cc",
+  },
+  {
+    title: "Follow Us",
+    link: "https://twitter.com/nomis_protocol",
+  },
+];
+
+export const footerMenu = [
+  {
+    title: "Docs",
+    link: "https://api.nomis.cc",
+  },
+  {
+    title: "Contact Us",
+    link: "mailto:gm@nomis.cc",
+  },
+  {
+    title: "Twitter",
+    link: "https://twitter.com/nomis_protocol",
+  },
+  {
+    title: "Telegram",
+    link: "https://t.me/nomis_protocol",
+  },
+];
