08d63d743ddd5a7841d0943afaf96534ecd8ab73
Ankush263 <ankushbanik263@gmail.com>
2022-11-06 19:49:07 +0530

    Update pinata

--- FOW FR/fow_client/pages/api/pinata.ts
+++ FOW FR/fow_client/pages/api/pinata.ts
@@ -2,22 +2,23 @@
 
 const key = process.env.NEXT_PUBLIC_PINATA_KEY
 const secret = process.env.NEXT_PUBLIC_PINATA_SECRET
-// const key = "a05892eb2f6d9fb84e63"
-// const secret = "6b48a733a482dd4bd019e7ff8d24fa89c63f06155ca624d4c0b20aaee1ef36c3"
 
 const axios = require('axios');
 const FormData = require('form-data');
 
 export const uploadRescueJSONToIPFS = async(JSONBody: {
-        language: string;
-        name: string;
-        numberOfPeople: string;
-        ageGroup: string;
-        reason: string;
-        location: string;
-        phoneNo: string;
-        moreInfo: string;
-
+    language: string;
+    name: string;
+    numberOfPeople: string;
+    lessthen1: string;
+    oneTofive: string;
+    sixTothirteen: string;
+    forAdults: string;
+    above60: string;
+    reason: string;
+    location: string;
+    phoneNo: string;
+    moreInfo: string;
     }) => {
     const url = `https://api.pinata.cloud/pinning/pinJSONToIPFS`;
     //making axios POST request to Pinata ⬇️
