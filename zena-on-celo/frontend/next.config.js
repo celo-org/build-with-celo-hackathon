78b747c8bc18c60cb1799b6c4a0dcaf5929eb873
rike <hello@rike.dev>
2022-11-08 16:26:47 +0100

    update config

--- zena-on-celo/frontend/next.config.js
+++ zena-on-celo/frontend/next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
