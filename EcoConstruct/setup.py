57cbb9b85136c39b2a2da0b2098e9340474a6c7d
slope-expedition <114743891+slope-expedition@users.noreply.github.com>
2022-11-13 13:53:05 +0000

    Delete setup.py
    Moving from python to react Js

--- EcoConstruct/setup.py
+++
@@ -1,6 +0,0 @@
-from celo_sdk.kit import Kit
-
-kit = Kit('https://alfajores-forno.celo-testnet.org')
-currency_address = kit.base_wrapper.registry.load_contract_by_name('StableToken')['address']
-kit.wallet_fee_currency = currency_address
-print("checked")
