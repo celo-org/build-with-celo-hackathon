2bc20daa2282c43d61774bb3d69b0a82dccec0ed
Ivelin Eftimoff <109984548+ftimoff@users.noreply.github.com>
2022-11-15 21:28:10 +0300

    Frontend public

---
+++ Dygnify/frontend/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#6047FF",
+          secondary: "#5375FE",
+          accent: "#37cdbe",
+          neutral: "#A0ABBB",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+};
